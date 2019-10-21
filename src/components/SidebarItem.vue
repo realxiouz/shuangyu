15b81587e0a7df631a2709b33020480bc5d76cc1
付海波 <541538356@qq.com>
2019-10-21 20:58:58 +0800

    admin

--- src/components/SidebarItem.vue
+++ src/components/SidebarItem.vue
@@ -1,30 +1,26 @@
-<template>
-  <div>
-    <el-submenu v-if="item.children && item.children.length>0" :index="item.path">
-      <template slot="title">
-        <i class="el-icon-menu"></i>
-        <span slot="title">{{ item.title }}</span>
-      </template>
+<template v-if="item.children">
+  <el-submenu v-if="item.children && item.children.length>0" :index="item.path">
+    <template slot="title">
+      <i class="el-icon-menu"></i>
+      <span slot="title">{{ item.title }}</span>
+    </template>
 
-      <template v-for="child in item.children">
-        <sidebar-item
-          v-if="child.children && child.children.length > 0"
-          :item="child"
-          :key="child.path"
-        />
-        <el-menu-item v-else :key="child.path" :index="child.path">
-          <i class="el-icon-location"></i>
-          <span slot="title">{{ child.title }}</span>
-        </el-menu-item>
-      </template>
-    </el-submenu>
-    <template v-else>
-      <el-menu-item :index="item.path">
-        <i class="el-icon-menu"></i>
-        <span slot="title">{{ item.title }}</span>
+    <template v-for="child in item.children">
+      <sidebar-item
+        v-if="child.children && child.children.length > 0"
+        :item="child"
+        :key="child.path"
+      />
+      <el-menu-item v-else :key="child.path" :index="child.path">
+        <i class="el-icon-location"></i>
+        <span slot="title">{{ child.title }}</span>
       </el-menu-item>
     </template>
-  </div>
+  </el-submenu>
+  <el-menu-item v-else :index="item.path">
+    <i class="el-icon-menu"></i>
+    <span slot="title">{{ item.title }}</span>
+  </el-menu-item>
 </template>
 
 <script>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -62,14 +62,12 @@ export default {
 .el-footer {
   background-color: #b3c0d1;
   color: #333;
-  text-align: center;
   line-height: 60px;
 }
 
 .el-aside {
   background-color: #d3dce6;
   color: #304156;
-  text-align: center;
   overflow: hidden;
 }
 
