c7f9a1b13ed493ef7496d2d1ae57c476696f7585
jiangtixiang <642172707@qq.com>
2020-02-20 13:34:26 +0800

    角色管理

--- src/api/role.js
+++ src/api/role.js
@@ -1,24 +1,38 @@
-import request from '@/utils/request';
+import request from "@/utils/request";
 
-export function addOrUpdateOne(data) {
+export function save(data) {
   return request({
-    url: '/admin/role/add/orUpdate/one',
-    method: 'post',
+    url: "/admin/role/save",
+    method: "post",
     data
   });
 }
 
 export function removeOne(data) {
   return request({
-    url: '/admin/role/remove/one/'+data,
-    method: 'delete'
+    url: "/admin/role/remove/one/" + data,
+    method: "delete"
   });
 }
 
 
 export function getPageList(data) {
   return request({
-    url: '/admin/role/page/list/'+data.currentPage+"/"+data.pageSize,
-    method: 'get'
+    url: "/admin/role/page/list/" + data.currentPage + "/" + data.pageSize,
+    method: "get"
+  });
+}
+
+export function getApis() {
+  return request({
+    url: "/admin/api/get/all",
+    method: "get"
+  });
+}
+
+export function getNavs() {
+  return request({
+    url: "/admin/nav/get/all",
+    method: "get"
   });
 }
