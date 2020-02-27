179861f84a5cb7893be8706467dfe94d0a034569
cxn <2240241348@qq.com>
2020-02-27 13:59:00 +0800

    新加store/modules/dept.js

---
+++ src/store/modules/dept.js
@@ -0,0 +1,69 @@
+import { save, removeOne,getNavsTreeData} from '@/api/dept';
+import { getToken } from '@/utils/auth';
+
+
+
+const state = {
+  token: getToken(),
+  name: '',
+  avatar: ''
+};
+
+const mutations = {
+  SET_TOKEN: (state, token) => {
+    state.token = token;
+  },
+  SET_NAME: (state, name) => {
+    state.name = name;
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar;
+  }
+};
+
+const actions = {
+  save({ commit }, params){
+    return new Promise((resolve, reject) => {
+      save(params)
+        .then(response => {
+          //const { data } = response;
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  removeOne({ commit },data) {
+    return new Promise((resolve, reject) => {
+      removeOne(data)
+        .then(response => {
+          const { data } = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  getNavsTreeData({ commit }){
+    return new Promise((resolve, reject) => {
+      getNavsTreeData()
+        .then(response => {
+          const { data } = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+};
