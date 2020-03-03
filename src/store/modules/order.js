5340baff7b243839763ad44436a467f10955b15b
cxn <2240241348@qq.com>
2020-03-03 14:52:11 +0800

    modules/order.js

---
+++ src/store/modules/order.js
@@ -0,0 +1,106 @@
+import {getList, getOne, getPageList, getTotal, removeOne, save} from "@/api/order";
+import {getToken} from "@/utils/auth";
+
+
+const state = {
+  token: getToken(),
+  name: "",
+  avatar: ""
+};
+
+const mutations = {
+  SET_TOKEN: (state, token) => {
+    state.token = token;
+  },
+  SET_NAME: (state, name) => {
+    state.name = name;
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar;
+  }
+};
+
+const actions = {
+  save({commit}, params) {
+    return new Promise((resolve, reject) => {
+      save(params)
+        .then(response => {
+          //const { data } = response;
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getList({commit}, data) {
+    return new Promise((resolve, reject) => {
+      getList(data)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  removeOne({commit}, data) {
+    return new Promise((resolve, reject) => {
+      removeOne(data)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getPageList({commit}, params) {
+    const {pageFlag, pageSize, lastId, filter} = params;
+    return new Promise((resolve, reject) => {
+      getPageList(pageFlag, pageSize, lastId, filter)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  getTotal({commit}, params) {
+    return new Promise((resolve, reject) => {
+      getTotal(params)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getOne({commit}, data) {
+    return new Promise((resolve, reject) => {
+      getOne(data)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+};
