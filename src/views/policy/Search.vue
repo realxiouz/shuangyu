f2b098f024c1260e173026f3c4fbe1f1ea010eba
sunlinfan <linfansun@163.com>
2020-03-11 18:54:56 +0800

    政策

--- src/views/policy/Search.vue
+++ src/views/policy/Search.vue
@@ -1,10 +1,16 @@
 <template>
   <el-form :inline="true" :model="searchForm">
-    <el-form-item label="用户名">
-      <el-input v-model="searchForm.user" placeholder="应用名称"></el-input>
+    <el-form-item label="政策编码">
+      <el-input v-model="searchForm.policyCode" placeholder="政策编码"></el-input>
     </el-form-item>
-    <el-form-item label="域名">
-      <el-input v-model="searchForm.domain" placeholder="应用名称"></el-input>
+    <el-form-item label="航司">
+      <el-input v-model="searchForm.airlineCode" placeholder="航司"></el-input>
+    </el-form-item>
+    <el-form-item label="起飞城市">
+      <el-input v-model="searchForm.dpt" placeholder="起飞城市"></el-input>
+    </el-form-item>
+    <el-form-item label="到达城市">
+      <el-input v-model="searchForm.arr" placeholder="到达城市"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="$emit('onSearch',searchForm)">查询</el-button>
@@ -19,9 +25,11 @@
         name: 'policySearch',
         data() {
             return {
-                searchForm:{
-                    user:'',
-                    domain:''
+                searchForm: {
+                    policyCode: '',
+                    airlineCode: '',
+                    dpt: '',
+                    arr: ''
                 }
             }
         }
