c0b2db274745409c6ac0fdd05ae82761e6949dbb
李思韩 <1915844250@qq.com>
2020-03-12 14:51:59 +0800

    orederRule 页面布局2

--- src/components/SelectRoles.vue
+++ src/components/SelectRoles.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="tableData" style="width: 100%;margin-bottom:15px">
+      <el-table-column
+        type="selection"
+        width="55">
+      </el-table-column>
       <el-table-column
         prop="roleName"
         label="角色名称"
         align="center"
+        tooltip-effect="dark"
       ></el-table-column>
     </el-table>
   </div>
@@ -16,8 +21,27 @@
     props: ["firmId"],
     data() {
       return {
+        pageFlag: "next",
+        pageSize: 10,
+        lastId: "0",
         tableData: []
       };
+    },
+    created(){
+      this.loadData();
+    },
+    methods:{
+      loadData(){
+        this.$store
+          .dispatch("role/getPageList", {pageFlag: this.pageFlag, pageSize: this.pageSize, lastId: this.lastId, filter: {}})
+          .then(data => {
+            this.tableData = data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+
     }
   };
 </script>
