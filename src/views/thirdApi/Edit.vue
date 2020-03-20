64284e0afbfe5ead1e9100b85f91d176276f477b
李思韩 <1915844250@qq.com>
2020-03-20 14:01:22 +0800

    第三方api：search修改

--- src/views/thirdApi/Edit.vue
+++ src/views/thirdApi/Edit.vue
@@ -1,79 +1,74 @@
 <template>
   <div>
-    <el-form ref="form" :rules="rules" :model="formData" label-width="110px">
-      <input type="hidden" v-model="formData.apiId"/>
-      <el-form-item label="第三方平台">
+    <el-form ref="form" :model="formData" label-width="110px" size="mini">
+      <input type="hidden" v-model="formData.apiId" />
+      <el-form-item label="第三方平台:">
         <el-input v-model="formData.thirdId"></el-input>
       </el-form-item>
-      <el-form-item label="URL">
+      <el-form-item label="URL:">
         <el-input v-model="formData.url"></el-input>
       </el-form-item>
-      <el-form-item label="方法">
+      <el-form-item label="方法:">
         <el-input v-model="formData.method"></el-input>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
-      <el-button @click="$emit('onCancel')">取 消</el-button>
-      <el-button type="primary" @click="handleSave">确 定</el-button>
+      <el-button size="mini" type="primary" @click="$emit('onCancel')">取 消</el-button>
+      <el-button size="mini" type="primary" @click="handleSave">确 定</el-button>
     </div>
-
   </div>
 </template>
 
 <script>
-
-    function defaultData() {
-        return {
-            apiId: '',
-            thirdId: '',
-            url: '',
-            method: ''
-        };
+function defaultData() {
+  return {
+    apiId: "",
+    thirdId: "",
+    url: "",
+    method: ""
+  };
+}
+export default {
+  name: "apiEdit",
+  data() {
+    return {
+      formData: defaultData()
     };
-    export default {
-        name: 'apiEdit',
-        data() {
-            return {
-                formData: defaultData()
-            };
-        },
-        methods: {
-            handleSave() {
-                this.$refs['form'].validate((valid) => {
-                    if (valid) {
-                        this.$emit("onSave", this.formData);
-                    }
-                })
-            },
-            handleGetOne(id) {
-                if (id) {
-                    this.$store
-                        .dispatch("thirdApiService/getOne", {apiId: id})
-                        .then(data => {
-                            this.formData = data;
-                            this.dialogVisible = true;
-                        }).catch(error => {
-                        console.log(error);
-                    });
-                } else {
-                    this.formData = defaultData();
-                }
-            },
-        },
-        created() {
-            if (this.apiId) {
-                this.handleGetOne(this.apiId);
-            }
-        },
-        props: {
-            apiId: String,
+  },
+  methods: {
+    handleSave() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          this.$emit("onSave", this.formData);
         }
-
-    };
+      });
+    },
+    handleGetOne(id) {
+      if (id) {
+        this.$store
+          .dispatch("thirdApiService/getOne", { apiId: id })
+          .then(data => {
+            this.formData = data;
+            this.dialogVisible = true;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        this.formData = defaultData();
+      }
+    }
+  },
+  created() {
+    if (this.apiId) {
+      this.handleGetOne(this.apiId);
+    }
+  },
+  props: {
+    apiId: String
+  }
+};
 </script>
 
 <style scoped>
-  .line {
-    text-align: center;
-  }
 </style>
