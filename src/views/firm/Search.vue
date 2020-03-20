bafab36e29ff0b79361d4017a24656a7e30a09ef
李思韩 <1915844250@qq.com>
2020-03-20 11:32:16 +0800

    企业管理search修改

--- src/views/firm/Search.vue
+++ src/views/firm/Search.vue
@@ -1,27 +1,54 @@
 <template>
-  <div>
-    <el-input v-model="keyword" style="width: 300px; margin-left: 50px;" placeholder="请输入企业名称..">
-      <i class="el-icon-edit el-input__icon"
-         slot="suffix"
-         @click="handleIconClick">
-      </i>
-    </el-input>
-    <el-button type="primary" icon="el-icon-search" @click="$emit('onSearch',keyword)"></el-button>
-  </div>
+  <el-row type="flex" justify="space-between" align="bottom">
+    <el-col :xs="16" :sm="18" :md="18" :lg="20" :xl="20">
+      <el-form :model="formData" label-width="110px" size="mini">
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item label="企业名称：">
+            <el-input v-model="formData.keyword" placeholder="请输入企业名称.."></el-input>
+          </el-form-item>
+        </el-col>
+      </el-form>
+    </el-col>
+    <el-col :xs="8" :sm="6" :md="6" :lg="4" :xl="4" class="search-tools">
+      <el-button
+        icon="el-icon-search"
+        class="filter-item"
+        type="primary"
+        size="mini"
+        @click="$emit('onSearch', formData)"
+      >查询</el-button>
+      <el-button type="text" size="mini" @click="handleMore">
+        更多
+        <i :class="switchIcon"></i>
+      </el-button>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        keyword: ''
-      };
-    },
-    methods: {
-      /*清空搜索框内的关键字*/
-      handleIconClick() {
-        this.keyword = "";
+export default {
+  data() {
+    return {
+      more: false,
+      formData: {
+        keyword: ""
+      }
+    };
+  },
+
+  computed: {
+    switchIcon() {
+      if (!this.more) {
+        return "el-icon-arrow-down el-icon--right";
+      } else {
+        return "el-icon-arrow-up el-icon--right";
       }
     }
-  };
+  },
+  methods: {
+    handleMore() {
+      this.more = !this.more;
+    }
+  }
+};
 </script>
