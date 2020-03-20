0fe8dd04088a1a62bd8a41567396830ea99ac5d9
nuter <13170600305@163.com>
2020-03-20 14:20:10 +0800

    去哪儿订单管理模块。

---
+++ src/views/qunarOrderConfig/Edit.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <el-form :model="formData" label-width="110px">
+      <el-form-item label="代理商域名">
+        <el-input v-model="formData.domain"></el-input>
+      </el-form-item>
+      <el-form-item label="企业">
+        <el-input v-model="formData.firmId"></el-input>
+      </el-form-item>
+      <el-form-item label="平台">
+        <el-input v-model="formData.thirdId"></el-input>
+      </el-form-item>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.user"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.pass"></el-input>
+      </el-form-item>
+      <el-form-item label="服务器IP">
+        <el-input v-model="formData.ips"></el-input>
+      </el-form-item>
+    </el-form>
+    <div>
+      <el-button @click="$emit('onCancel')">取 消</el-button>
+      <el-button type="primary" @click="handleConfirm">确 定</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ["curNode", "update"],
+        data() {
+            return {
+                formData: {}
+            };
+        },
+        methods: {
+            /*表单默认加载数据*/
+            defaultFormData() {
+                return {
+                    domain: '',
+                    firmId: '',
+                    thirdId: '',
+                    user: '',
+                    pass: '',
+                    ips: ''
+                };
+            },
+            /*清除表单*/
+            clearForm() {
+                this.formData = this.defaultFormData();
+            },
+            /*对提交的数据进行类型格式*/
+            handleConfirm(){
+                this.$emit('onSave',this.formData);
+            },
+            initFormData(){
+                this.clearForm();
+                if (this.update){
+                    this.formData = this.curNode;
+                }
+            }
+        },
+        created() {
+            this.initFormData();
+        }
+    };
+</script>
---
+++ src/views/qunarOrderConfig/List.vue
@@ -0,0 +1,222 @@
+<template>
+  <el-container>
+    <el-main>
+      <qunar-order-config-search @onSearch="search"/>
+      <el-header>
+        <el-button type="primary" @click="handleAdd" style="margin-top: 10px;margin-left: 30px">添加</el-button>
+      </el-header>
+      <el-table :data="tableData" style="width: 100%;margin-bottom: 20px;">
+        <el-table-column
+          prop="domain"
+          label="代理商域名"
+          width="280"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          prop="firmId"
+          label="企业"
+          width="280"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          prop="thirdId"
+          label="平台"
+          width="280"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="user"
+          label="用户名"
+          width="280"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="ips"
+          label="服务器IP"
+          width="280"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          align="center">
+          <template slot-scope="scope">
+            <el-button @click="handleEdit(scope.row)" type="primary" size="mini">编辑</el-button>
+            <el-button @click="handleDelete(scope.row)" type="danger" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        background
+        layout="total,prev,next"
+        prev-text="上一页"
+        next-text="下一页"
+        :page-size="pageSize"
+        :total="total"
+        @prev-click="handlePrevClick"
+        @next-click="handleNextClick">
+      </el-pagination>
+
+      <el-dialog title="第三方平台账号信息" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
+        <qunar-order-config-edit v-if="dialogVisible" :curNode="curNode" :update="update" @onSave="handleSave"
+                            @onCancel="handleCancel"/>
+      </el-dialog>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+    import qunarOrderConfigSearch from './Search'
+    import qunarOrderConfigEdit from './Edit'
+
+    export default {
+        data() {
+            return {
+                dialogVisible: false,
+                tableData: [],
+                /*记录当前进行操作的节点*/
+                curNode: {},
+                update: false,
+                pageFlag: "next",
+                pageSize: 10,
+                lastId: "blank",
+                total: 0
+            };
+        },
+        methods: {
+            /*加载数据列表*/
+            loadData() {
+                this.$store
+                    .dispatch('qunarOrderConfig/getTotal', {filter: {}})
+                    .then(data => {
+                        this.total = data.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+                this.$store
+                    .dispatch('qunarOrderConfig/getPageList', {
+                        pageFlag: this.pageFlag,
+                        pageSize: this.pageSize,
+                        lastId: this.lastId,
+                        filter: {}
+                    })
+                    .then(data => {
+                        this.tableData = data.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            /*输入条件时可进行条件查询*/
+            search(keyword) {
+                this.$store
+                    .dispatch('qunarOrderConfig/getTotal', {filter: keyword ? {username: keyword} : {}})
+                    .then(data => {
+                        this.total = data.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+                this.$store
+                    .dispatch('qunarOrderConfig/getPageList', {
+                        pageFlag: this.pageFlag,
+                        pageSize: this.pageSize,
+                        lastId: this.lastId,
+                        filter: keyword ? {username: keyword} : {}
+                    })
+                    .then(data => {
+                        this.tableData = data.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            /*添加记录*/
+            handleAdd() {
+                this.dialogVisible = true;
+                this.curNode = {};
+                this.update = false;
+            },
+            /*添加记录时完成数据填写或编辑记录时，点击对数据进行保存*/
+            handleSave(formData) {
+                this.dialogVisible = false;
+
+                let url = '';
+                if (this.update) {
+                    url = 'qunarOrderConfig/updateOne';
+                } else {
+                    url = 'qunarOrderConfig/addOne';
+                }
+                this.$store
+                    .dispatch(url, formData)
+                    .then(() => {
+                        this.loadData();
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            handleCancel() {
+                this.dialogVisible = false;
+            },
+            /*点击记录进行编辑*/
+            handleEdit(row) {
+                this.dialogVisible = true;
+                this.curNode = row;
+                this.update = true;
+            },
+            /*对员工进行删除*/
+            handleDelete(row) {
+                this.open(this.delete, row.domain, '此操作将删除该第三方平台账号信息, 是否继续?');
+            },
+            /*根据订单域名删除用户*/
+            delete(domain) {
+                this.$store
+                    .dispatch('qunarOrderConfig/removeOne', {domain: domain})
+                    .then(() => {
+                        this.loadData();
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            /*前翻页*/
+            handlePrevClick() {
+                this.pageFlag = "prev";
+                this.lastId = this.tableData[0].domain;
+                this.loadData();
+            },
+            /*翻后页*/
+            handleNextClick() {
+                this.pageFlag = "next";
+                this.lastId = this.tableData[this.tableData.length - 1].domain;
+                this.loadData();
+            },
+            open(func, data, message) {
+                this.$confirm(message, '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    func(data);
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            }
+        },
+        created() {
+            this.loadData();
+        },
+        components: {
+            qunarOrderConfigSearch,
+            qunarOrderConfigEdit
+        }
+    }
+</script>
