781b9486a93442bd62ef5a423f8355c27ce1f70d
李思韩 <1915844250@qq.com>
2020-03-20 13:49:53 +0800

    TGQ调度：search修改

--- src/views/tgqProduct/List.vue
+++ src/views/tgqProduct/List.vue
@@ -1,42 +1,40 @@
 <template>
   <div class="app-container">
-    <tgq-product-search @onSearch="handleSearch" @onAdd="add"></tgq-product-search>
-    <el-table :data="tableData" style="width: 100%;margin-bottom: 20px;" border
-              default-expand-all>
-      <el-table-column
-        prop="cron"
-        label="时间表达式"
-        width="150"
-      ></el-table-column>
-      <el-table-column
-        prop="schedulerName"
-        label="调度名称"
-      ></el-table-column>
-      <el-table-column
-        prop="remark"
-        label="备注"
-      ></el-table-column>
-      <el-table-column
-        prop="status"
-        label="调度程序状态"
-        width="120"
-      >
+    <tgq-product-search @onSearch="handleSearch"></tgq-product-search>
+    <el-row style="margin-bottom:15px;">
+      <el-button icon="el-icon-plus" type="primary" size="mini" @click="handleAdd">添加</el-button>
+    </el-row>
+    <el-table :data="tableData" style="width: 100%;margin-bottom: 20px;" fit default-expand-all>
+      <el-table-column prop="cron" label="时间表达式" width="150"></el-table-column>
+      <el-table-column prop="schedulerName" label="调度名称"></el-table-column>
+      <el-table-column prop="remark" label="备注"></el-table-column>
+      <el-table-column prop="status" label="调度程序状态" width="120">
         <template slot-scope="scope">
           <span v-if="scope.row.status==1" style="color: green">启动</span>
           <span v-else style="color: red">停止</span>
         </template>
       </el-table-column>
-      <el-table-column
-        fixed="right"
-        label="操作"
-        align="center"
-        width="300">
+      <el-table-column fixed="right" label="操作" align="center" width="300">
         <template slot-scope="scope">
-          <el-button v-if="scope.row.status==1" @click="handleStop(scope.row.schedulerId)" type="danger" size="mini">停止</el-button>
-          <el-button v-else @click="handleStart(scope.row.schedulerId)" type="primary" size="mini">启动</el-button>
+          <el-button
+            v-if="scope.row.status==1"
+            @click="handleStop(scope.row.schedulerId)"
+            type="danger"
+            size="mini"
+          >停止</el-button>
+          <el-button
+            v-else
+            @click="handleStart(scope.row.schedulerId)"
+            type="primary"
+            size="mini"
+          >启动</el-button>
           <el-button @click="handleEdit(scope.row)" type="primary" size="mini">编辑</el-button>
           <el-button @click="handleCopy(scope.row)" type="primary" size="mini">复制</el-button>
-          <el-button @click.native.prevent="removeOne(scope.row.schedulerId)" type="danger" size="mini">删除</el-button>
+          <el-button
+            @click.native.prevent="removeOne(scope.row.schedulerId)"
+            type="danger"
+            size="mini"
+          >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -44,117 +42,123 @@
 </template>
 
 <script>
-  import tgqProductSearch from "./Search.vue";
+import tgqProductSearch from "./Search.vue";
 
-  export default {
-    name: "tgqProductList",
-    data() {
-      return {
-        schedulerId: '',
-        searchForm: {},
-        tableData: []
-      };
-    },
-    methods: {
-      handleStart(schedulerId) {
-        if (schedulerId){
-          this.$store
-            .dispatch("tgqProduct/start", {schedulerId: schedulerId})
-            .then(() => {
-              this.loadData();
-            }).catch(error => {
-            console.log(error);
-          });
-        }
-      },
-      handleStop(schedulerId) {
-        if (schedulerId){
-          this.$store
-            .dispatch("tgqProduct/stop", {schedulerId: schedulerId})
-            .then(() => {
-              this.loadData();
-            }).catch(error => {
-            console.log(error);
-          });
-        }
-      },
-      handleEdit(row) {
-        if (row.status==1){
-          this.$confirm('是否确定停止此产品编辑?', '提示', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
-          }).then(() => {
-            this.$store
-              .dispatch('tgqProduct/stop', {schedulerId: row.schedulerId})
-              .then(() => {
-                row.copyFlag = false;
-                this.$router.push({name: 'tgqProductEdit', params: row});
-              })
-              .catch(error => {
-                console.log(error);
-              });
-          }).catch(err => {
-            console.error(err);
-          });
-        }else {
-          row.copyFlag = false;
-          this.$router.push({name: 'tgqProductEdit', params: row});
-        }
-      },
-      handleCopy(row) {
-        row.copyFlag = true;
-        this.$router.push({name: 'tgqProductEdit', params: row});
-      },
-      add() {
-        this.$router.push('/tgq/product/edit');
-      },
-      loadData() {
+export default {
+  name: "tgqProductList",
+  data() {
+    return {
+      schedulerId: "",
+      searchForm: {},
+      tableData: []
+    };
+  },
+  methods: {
+    handleStart(schedulerId) {
+      if (schedulerId) {
         this.$store
-          .dispatch('tgqProduct/getList', {
-            searchForm: this.searchForm
-          })
-          .then(data => {
-            this.tableData = data;
+          .dispatch("tgqProduct/start", { schedulerId: schedulerId })
+          .then(() => {
+            this.loadData();
           })
           .catch(error => {
             console.log(error);
           });
-      },
-      handleSearch(params) {
-        this.searchForm = params;
-        this.loadData();
-      },
-      removeOne(id) {
-        this.$confirm('是否确定删除TGQ信息?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
+      }
+    },
+    handleStop(schedulerId) {
+      if (schedulerId) {
+        this.$store
+          .dispatch("tgqProduct/stop", { schedulerId: schedulerId })
+          .then(() => {
+            this.loadData();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    },
+    handleEdit(row) {
+      if (row.status == 1) {
+        this.$confirm("是否确定停止此产品编辑?", "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        })
+          .then(() => {
+            this.$store
+              .dispatch("tgqProduct/stop", { schedulerId: row.schedulerId })
+              .then(() => {
+                row.copyFlag = false;
+                this.$router.push({ name: "tgqProductEdit", params: row });
+              })
+              .catch(error => {
+                console.log(error);
+              });
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      } else {
+        row.copyFlag = false;
+        this.$router.push({ name: "tgqProductEdit", params: row });
+      }
+    },
+    handleCopy(row) {
+      row.copyFlag = true;
+      this.$router.push({ name: "tgqProductEdit", params: row });
+    },
+    handleAdd() {
+      this.$router.push("/tgq/product/edit");
+    },
+    loadData() {
+      this.$store
+        .dispatch("tgqProduct/getList", {
+          searchForm: this.searchForm
+        })
+        .then(data => {
+          this.tableData = data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    handleSearch(params) {
+      this.searchForm = params;
+      this.loadData();
+    },
+    removeOne(id) {
+      this.$confirm("是否确定删除TGQ信息?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
           this.$store
-            .dispatch('tgqProduct/removeOne', {schedulerId: id})
+            .dispatch("tgqProduct/removeOne", { schedulerId: id })
             .then(() => {
               this.loadData();
             })
             .catch(error => {
               console.log(error);
             });
-        }).catch(err => {
+        })
+        .catch(err => {
           console.error(err);
         });
-      },
-    },
-    created() {
-      this.loadData();
-    },
-    components: {
-      tgqProductSearch
     }
-  };
+  },
+  created() {
+    this.loadData();
+  },
+  components: {
+    tgqProductSearch
+  }
+};
 </script>
 
 <style scoped>
-  .line {
-    text-align: center;
-  }
+.line {
+  text-align: center;
+}
 </style>
