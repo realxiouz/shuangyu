44ab7430b76083403dc2cf1f8d1c2f963559a656
李思韩 <1915844250@qq.com>
2020-03-23 10:18:15 +0800

    样式修改 修改

--- src/views/policy/Search.vue
+++ src/views/policy/Search.vue
@@ -3,27 +3,27 @@
     <el-col :xs="16" :sm="18" :md="18" :lg="20" :xl="20">
       <el-form :model="formData" label-width="110px" size="mini">
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="政策编码">
+          <el-form-item label="政策编码:">
             <el-input class="filter-item" v-model="formData.policyCode" placeholder="政策编码"></el-input>
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="航司">
+          <el-form-item label="航司:">
             <el-input class="filter-item" v-model="formData.airlineCode" placeholder="航司"></el-input>
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="舱位">
+          <el-form-item label="舱位:">
             <el-input class="filter-item" v-model="formData.cabin" placeholder="舱位"></el-input>
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item v-show="more" label="起飞城市">
+          <el-form-item v-show="more" label="起飞城市:">
             <el-input class="filter-item" v-model="formData.dpt" placeholder="起飞城市"></el-input>
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item v-show="more" label="到达城市">
+          <el-form-item v-show="more" label="到达城市:">
             <el-input class="filter-item" v-model="formData.arr" placeholder="到达城市"></el-input>
           </el-form-item>
         </el-col>
@@ -31,7 +31,7 @@
           <el-form-item v-show="more" label="销售起始日期:">
             <el-date-picker
               type="date"
-              placeholder="选择日期"
+              placeholder="选择销售起始日期"
               v-model="formData.sellStartDate"
               style="width: 100%;"
               value-format="yyyy-MM-dd"
@@ -42,7 +42,7 @@
           <el-form-item v-show="more" label="销售结束日期:">
             <el-date-picker
               type="date"
-              placeholder="选择日期"
+              placeholder="选择销售结束日期"
               v-model="formData.sellEndDate"
               style="width: 100%;"
               value-format="yyyy-MM-dd"
@@ -53,7 +53,7 @@
           <el-form-item v-show="more" label="出发日期:">
             <el-date-picker
               type="date"
-              placeholder="选择日期"
+              placeholder="选择出发日期"
               v-model="formData.flightDate"
               style="width: 100%;"
               value-format="yyyy-MM-dd"
