b307345e103bb91168c0d2873264c9299c61591e
李思韩 <1915844250@qq.com>
2020-03-23 09:17:38 +0800

    firm 修改

--- src/views/firm/List.vue
+++ src/views/firm/List.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tree-node">
     <firm-search @onSearch="handleSearch"></firm-search>
-    <el-row style="margin-bottom:15px;">
+    <el-row style="margin-bottom:15px; margin-left:25px;">
       <el-button icon="el-icon-plus" type="primary" size="mini" @click="handleAdd">添加</el-button>
     </el-row>
     <el-table :data="tableData" row-key="firmId" :tree-props="tableProps">
