0dfed83beaa6699b02ecd40e71e929ec98381512
杨绍宾 <1182705849@qq.com>
2020-03-24 13:11:13 +0800

    airport

---
+++ src/views/orderStaff/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-tree
+        :data="treeData"
+        :props="treeProps"
+        :highlight-current="true"
+        :expand-on-click-node="false"
+        @node-click="handleNodeClick">
+      </el-tree>
+    </el-aside>
+    <staff-form :curNode="curNode" :staffAddVisible="staffAddVisible" style="width: 100%"/>
+  </el-container>
+</template>
+
+<script>
+  import staffForm from "./Edit";
+
+  export default {
+    data() {
+      return {
+        staffAddVisible: true,
+        curNode: null,
+        treeData: [],
+        treeProps: {
+          label: 'deptName',
+          children: 'children'
+        }
+      }
+    },
+    methods: {
+      loadTreeData() {
+        this.$store
+          .dispatch('dept/getList', {filters: {firmId: '78ac36e7f76747a7aa069cd22177577f'}})
+          .then(data => {
+            this.treeData = data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+      handleNodeClick(data) {
+        this.staffAddVisible = false;
+        this.curNode = data;
+      }
+    },
+    mounted() {
+      this.loadTreeData();
+    },
+    components: {
+      staffForm
+    }
+  };
+</script>
