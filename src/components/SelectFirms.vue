0d2e107194afc6237c237e372e10ee1ee84e3cfe
李思韩 <1915844250@qq.com>
2020-03-24 16:09:36 +0800

    样式修改

--- src/components/SelectFirms.vue
+++ src/components/SelectFirms.vue
@@ -1,13 +1,39 @@
 <template>
-
+  <div>
+    <el-table
+      :data="firms"
+      style="width: 100%"
+      size="mini"
+      highlight-current-row
+      @current-change="handleCurrentChange"
+    >
+      <el-table-column prop="firmName" label="企业名称" align="center"></el-table-column>
+    </el-table>
+    <div slot="footer" class="dialog-footer" style=" margin-top:15px;text-align:right;">
+      <el-button size="mini" type="primary" @click="handleSave">确 定</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "SelectFirms"
-  };
+export default {
+  name: "SelectFirms",
+  props: ["firms"],
+  data() {
+    return {
+      firmId: ""
+    };
+  },
+  methods: {
+    handleCurrentChange(row) {
+      this.firmId = row.firmId;
+    },
+    handleSave() {
+      this.$emit("onSave", this.firmId);
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
