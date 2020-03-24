75d9a5442b0c69ce444bf87e93e7af260a0e4a42
sunlinfan <linfansun@163.com>
2020-03-24 17:21:49 +0800

    订单报表

--- src/views/orderReport/List.vue
+++ src/views/orderReport/List.vue
@@ -8,13 +8,27 @@
         <el-button icon="el-icon-plus" type="primary" size="mini" @click="handleAdd">添加</el-button>
       </el-row>
       <el-table :data="tableData" size="mini" highlight-current-row style="width: 100%;" border fit>
-        <el-table-column prop="orderNo" label="订单号" width="160"></el-table-column>
-        <el-table-column prop="rootOrderNo" label="销售出票单号" width="100"></el-table-column>
-        <el-table-column prop="orderSource" label="订单来源" width="100"></el-table-column>
-        <el-table-column prop="sourceOrderNo" label="订单来源单号" width="100"></el-table-column>
-        <el-table-column prop="policyCode" label="政策代码" width="100"></el-table-column>
-        <el-table-column prop="policyType" label="政策类型" width="100"></el-table-column>
+        <el-table-column prop="orderNo" label="订单号" width="180"></el-table-column>
+        <el-table-column prop="policyCode" label="政策代码" width="150"></el-table-column>
         <el-table-column prop="statusName" label="订单状态" width="100"></el-table-column>
+        <el-table-column label="订单日期" width="110" align="center">
+          <template slot-scope="scope">
+            <i v-if="scope.row.createTime" class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ formatDate(scope.row.createTime,'YYYY-MM-DD') }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="航班号" width="100" align="center">
+          <template slot-scope="scope">
+            <i v-if="scope.row.flights"></i>
+            <span style="margin-left: 10px">{{ formatFlightNo(scope.row.flights)}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="航班日期" width="110" align="center">
+          <template slot-scope="scope">
+            <i v-if="scope.row.flights"></i>
+            <span style="margin-left: 10px">{{ formatFlightDate(scope.row.flights)}}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="receivable" label="应收" width="100"></el-table-column>
         <el-table-column prop="payable" label="应付" width="100"></el-table-column>
         <el-table-column prop="receipt" label="实收" width="100"></el-table-column>
@@ -35,7 +49,8 @@
               @click.native.prevent="handleRemove(scope.row.deptId)"
               type="danger"
               size="mini"
-            >删除</el-button>
+            >删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -55,122 +70,135 @@
   </div>
 </template>
 <script>
-import orderReportSearch from "./Search.vue";
+    import orderReportSearch from "./Search.vue";
 
-export default {
-  name: "orderReportList",
-  data() {
-    return {
-      currentPage: 1,
-      pageSize: 10,
-      total: 0,
-      dialogVisible: false,
-      tableData: [],
-      searchParams: {}
+    export default {
+        name: "orderReportList",
+        data() {
+            return {
+                currentPage: 1,
+                pageSize: 10,
+                total: 0,
+                dialogVisible: false,
+                tableData: [],
+                searchParams: {}
+            };
+        },
+        methods: {
+            handleSizeChange: function (size) {
+                this.pageSize = size;
+                this.loadData(this.searchParams);
+            },
+            prevClick(page) {
+                this.currentPage = page;
+                this.loadData(this.searchParams);
+            },
+            nextClick(page) {
+                this.currentPage = page;
+                this.loadData(this.searchParams);
+            },
+            loadData(params) {
+                params.currentPage = this.currentPage;
+                params.pageSize = this.pageSize;
+                this.$store
+                    .dispatch("orderReport/getList", {
+                        filters: params
+                    })
+                    .then(data => {
+                        if (data) {
+                            this.tableData = data;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            loadTotal(params) {
+                this.$store
+                    .dispatch("orderReport/getTotal", {
+                        filters: params
+                    })
+                    .then(data => {
+                        this.total = data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            handleAdd() {
+                this.dialogVisible = true;
+            },
+            handleSearch(params) {
+                if (!params) {
+                    params = {};
+                    this.searchParams = params;
+                    this.loadData(this.searchParams);
+                    this.loadTotal(this.searchParams);
+                } else {
+                    const newParams = {};
+                    if (params.receivable) {
+                        newParams.receivable = params.receivable;
+                    }
+                    if (params.payable) {
+                        newParams.payable = params.payable;
+                    }
+                    if (params.orderNo) {
+                        newParams.orderNo = params.orderNo.toLocaleLowerCase();
+                    }
+                    if (params.receipt) {
+                        newParams.receipt = params.receipt;
+                    }
+                    if (params.payment) {
+                        newParams.payment = params.payment;
+                    }
+                    if (params.systemProfit) {
+                        newParams.systemProfit = params.systemProfit;
+                    }
+                    this.searchParams = newParams;
+                    this.loadData(this.searchParams);
+                    this.loadTotal(this.searchParams);
+                }
+            },
+            handleCancel() {
+                this.dialogVisible = false;
+            },
+            handleSave() {
+            },
+            /*初始化用工列表中的生日日期格式*/
+            initDate(dateStr, format) {
+                if (null != dateStr) {
+                    let date = new Date(dateStr);
+                    return this.$moment(date).format(format);
+                } else {
+                    return "";
+                }
+            },
+            formatFlightDate(data) {
+                if (!data || data.length == 0) {
+                    return "";
+                }
+                return this.initDate(data[0].flightDate, "YYYY-MM-DD");
+            },
+            formatFlightNo(data) {
+                if (!data || data.length == 0) {
+                    return "";
+                }
+                return data[0].flightCode;
+            },
+        },
+        components: {
+            orderReportSearch
+        },
+        computed: {
+            formatDate() {
+                return function (dateStr, format) {
+                    return this.initDate(dateStr, format);
+                };
+            }
+        },
+        created() {
+            this.loadData(this.searchParams);
+            this.loadTotal();
+        }
     };
-  },
-  methods: {
-    handleSizeChange: function(size) {
-      this.pageSize = size;
-      this.loadData(this.searchParams);
-    },
-    prevClick(page) {
-      this.currentPage = page;
-      this.loadData(this.searchParams);
-    },
-    nextClick(page) {
-      this.currentPage = page;
-      this.loadData(this.searchParams);
-    },
-    loadData(params) {
-      params.currentPage = this.currentPage;
-      params.pageSize = this.pageSize;
-      this.$store
-        .dispatch("orderReport/getList", {
-          filters: params
-        })
-        .then(data => {
-          if (data) {
-            this.tableData = data;
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    loadTotal(params) {
-      this.$store
-        .dispatch("orderReport/getTotal", {
-          filters: params
-        })
-        .then(data => {
-          this.total = data;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    handleAdd() {
-      this.dialogVisible = true;
-    },
-    handleSearch(params) {
-      if (!params) {
-        params = {};
-        this.searchParams = params;
-        this.loadData(this.searchParams);
-        this.loadTotal(this.searchParams);
-      } else {
-        const newParams = {};
-        if (params.receivable) {
-          newParams.receivable = params.receivable;
-        }
-        if (params.payable) {
-          newParams.payable = params.payable;
-        }
-        if (params.orderNo) {
-          newParams.orderNo = params.orderNo.toLocaleLowerCase();
-        }
-        if (params.receipt) {
-          newParams.receipt = params.receipt;
-        }
-        if (params.payment) {
-          newParams.payment = params.payment;
-        }
-        if (params.systemProfit) {
-          newParams.systemProfit = params.systemProfit;
-        }
-        this.searchParams = newParams;
-        this.loadData(this.searchParams);
-        this.loadTotal(this.searchParams);
-      }
-    },
-    handleCancel() {
-      this.dialogVisible = false;
-    },
-    handleSave() {},
-    /*初始化用工列表中的生日日期格式*/
-    initDate(dateStr, format) {
-      if (null != dateStr) {
-        let date = new Date(dateStr);
-        return this.$moment(date).format(format);
-      } else {
-        return "";
-      }
-    }
-  },
-  components: {
-    orderReportSearch
-  },
-  computed: {
-    formatDate() {
-      return function(dateStr, format) {
-        return this.initDate(dateStr, format);
-      };
-    }
-  },
-  created() {
-    this.loadData(this.searchParams);
-    this.loadTotal();
-  }
-};
 </script>
