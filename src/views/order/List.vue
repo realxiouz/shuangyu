d51003988d4f0378c730cb0d0535087717149b45
sunlinfan <linfansun@163.com>
2020-03-30 17:25:40 +0800

    orderTypeName

--- src/views/order/List.vue
+++ src/views/order/List.vue
@@ -22,21 +22,21 @@
 
         <el-table-column prop="orderNo" label="订单号" width="180" align="center"></el-table-column>
         <el-table-column prop="policyCode" label="政策代码" align="center"></el-table-column>
-        <el-table-column prop="statusName" label="订单状态" width="80" align="center"></el-table-column>
+        <el-table-column prop="status" label="订单状态" width="80" align="center"></el-table-column>
         <el-table-column label="订单日期" width="100" align="center">
           <template slot-scope="scope">
             <i v-if="scope.row.createTime"></i>
             <span style="margin-left: 10px">{{ formatDate(scope.row.createTime,'YYYY-MM-DD') }}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="categoryName" label="订单分类" width="80" align="center"></el-table-column>
+        <el-table-column prop="category" label="订单分类" width="80" align="center"></el-table-column>
         <el-table-column label="乘客" align="center" width="200">
           <template slot-scope="scope">
             <i v-if="scope.row.passengers"></i>
             <span style="margin-left: 10px">{{ formatPassengers(scope.row.passengers)}}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="voyageTypeName" label="航程类型" width="80" align="center"></el-table-column>
+        <el-table-column prop="voyageType" label="航程类型" width="80" align="center"></el-table-column>
         <el-table-column label="航班号" width="80" align="center">
           <template slot-scope="scope">
             <i v-if="scope.row.flights"></i>
