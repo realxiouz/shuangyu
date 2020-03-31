fb273aa1529784e36d4e334e7bd22886fd749bef
李思韩 <1915844250@qq.com>
2020-03-31 13:41:02 +0800

    添加loading

--- src/views/firm/List.vue
+++ src/views/firm/List.vue
@@ -7,7 +7,13 @@
       <el-row style="margin-bottom:15px; margin-left:25px;">
         <el-button icon="el-icon-plus" type="primary" size="mini" @click="handleAdd">添加</el-button>
       </el-row>
-      <el-table size="mini" :data="tableData" row-key="firmId" :tree-props="tableProps">
+      <el-table
+        v-loading="loading"
+        size="mini"
+        :data="tableData"
+        row-key="firmId"
+        :tree-props="tableProps"
+      >
         <el-table-column prop="firmName" label="企业名称" align="center" sortable width="100"></el-table-column>
         <el-table-column prop="firmCode" label="企业代码" align="center" sortable width="100"></el-table-column>
         <el-table-column prop="location" label="机构所在地" align="center"></el-table-column>
@@ -29,157 +35,161 @@
         width="29%"
         :close-on-click-modal="false"
       >
-        <firm-edit :curNode="curNode" @onSave="handleSave" @onCancel="handleCancel"/>
+        <firm-edit :curNode="curNode" @onSave="handleSave" @onCancel="handleCancel" />
       </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
-    import firmEdit from './Edit';
-    import firmSearch from './Search';
+import firmEdit from "./Edit";
+import firmSearch from "./Search";
 
-    export default {
-        data() {
-            return {
-                dialogVisible: false,
-                rootNav: false,
-                tableData: [],
-                curNode: {},
-                tableProps: {
-                    hasChildren: 'xxx',
-                    children: 'children'
-                }
-            };
-        },
-        methods: {
-            /*加载企业列表*/
-            loadData() {
-                this.$store
-                    .dispatch('firm/getList', {filter: {}})
-                    .then(data => {
-                        this.tableData = data;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            /*根据关键字进行企业搜索*/
-            handleSearch(params) {
-                this.$store
-                    .dispatch('firm/getList', {params})
-                    .then(data => {
-                        this.tableData = data;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            handleAdd() {
-                this.rootNav = true;
-                this.dialogVisible = true;
-
-                this.curNode = {};
-            },
-            /*企业的添加、编辑保存*/
-            handleSave(formData) {
-                this.dialogVisible = false;
-
-                 if (formData.firmId != '') {
-                     this.$store
-                         .dispatch('firm/updateOne', {firm: formData})
-                         .then(() => {
-                             this.loadData();
-                         })
-                         .catch(error => {
-                             console.log(error);
-                         });
-                 } else {
-                     if (this.rootNav) {
-                         //如果添加的顶级企业信息，对某些属性进行初始化
-                         formData.level = 0;
-                     } else {
-                         formData.pid = this.curNode.firmId;
-                         formData.level = this.curNode.level + 1;
-                     }
-
-                     this.$store
-                         .dispatch('firm/addOne', {firm: formData})
-                         .then(() => {
-                         this.$message({
-                            type: 'success',
-                            message: '企业账号已添加成功!超级管理员账号为企业联系人手机号或邮箱，密码已通过邮件发送给联系人'
-                        });
-                             this.loadData();
-                         })
-                         .catch(error => {
-                             console.log(error);
-                         });
-                 }
-            },
-            handleCancel() {
-                this.dialogVisible = false;
-            },
-            /*点击添加企业子节点按钮*/
-            handleAppend(idx, row) {
-                this.rootNav = false;
-                this.dialogVisible = true;
-
-                this.curNode = {};
-                this.curNode.firmId = row.firmId;
-                this.curNode.level = row.level;
-            },
-            /*点击编辑*/
-            handleEdit(index, row) {
-                this.curNode = row;
-                this.dialogVisible = true;
-            },
-            /*点击删除*/
-            handleDelete(index, row) {
-                this.open(
-                    this.remove,
-                    row.firmId,
-                    '此操作将删除该企业信息及子企业信息, 是否继续?'
-                );
-            },
-            /*删除企业数据*/
-            remove(params) {
-                this.$store
-                    .dispatch('firm/removeOne', {firmID: params})
-                    .then(() => {
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            open(func, data, message) {
-                this.$confirm(message, '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                })
-                    .then(() => {
-                        func(data);
-                        this.loadData();
-                        this.$message({
-                            type: 'success',
-                            message: '删除成功!'
-                        });
-                    })
-                    .catch(() => {
-                        this.$message({
-                            type: 'info',
-                            message: '已取消删除'
-                        });
-                    });
-            }
-        },
-        mounted() {
-            this.loadData();
-        },
-        components: {
-            firmEdit,
-            firmSearch
-        }
+export default {
+  data() {
+    return {
+      loading: true,
+      dialogVisible: false,
+      rootNav: false,
+      tableData: [],
+      curNode: {},
+      tableProps: {
+        hasChildren: "xxx",
+        children: "children"
+      }
     };
+  },
+  methods: {
+    /*加载企业列表*/
+    loadData() {
+      this.$store
+        .dispatch("firm/getList", { filter: {} })
+        .then(data => {
+          if (data) {
+            this.tableData = data;
+            this.loading = false;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    /*根据关键字进行企业搜索*/
+    handleSearch(params) {
+      this.$store
+        .dispatch("firm/getList", { params })
+        .then(data => {
+          this.tableData = data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    handleAdd() {
+      this.rootNav = true;
+      this.dialogVisible = true;
+
+      this.curNode = {};
+    },
+    /*企业的添加、编辑保存*/
+    handleSave(formData) {
+      this.dialogVisible = false;
+
+      if (formData.firmId != "") {
+        this.$store
+          .dispatch("firm/updateOne", { firm: formData })
+          .then(() => {
+            this.loadData();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        if (this.rootNav) {
+          //如果添加的顶级企业信息，对某些属性进行初始化
+          formData.level = 0;
+        } else {
+          formData.pid = this.curNode.firmId;
+          formData.level = this.curNode.level + 1;
+        }
+
+        this.$store
+          .dispatch("firm/addOne", { firm: formData })
+          .then(() => {
+            this.$message({
+              type: "success",
+              message:
+                "企业账号已添加成功!超级管理员账号为企业联系人手机号或邮箱，密码已通过邮件发送给联系人"
+            });
+            this.loadData();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    },
+    handleCancel() {
+      this.dialogVisible = false;
+    },
+    /*点击添加企业子节点按钮*/
+    handleAppend(idx, row) {
+      this.rootNav = false;
+      this.dialogVisible = true;
+
+      this.curNode = {};
+      this.curNode.firmId = row.firmId;
+      this.curNode.level = row.level;
+    },
+    /*点击编辑*/
+    handleEdit(index, row) {
+      this.curNode = row;
+      this.dialogVisible = true;
+    },
+    /*点击删除*/
+    handleDelete(index, row) {
+      this.open(
+        this.remove,
+        row.firmId,
+        "此操作将删除该企业信息及子企业信息, 是否继续?"
+      );
+    },
+    /*删除企业数据*/
+    remove(params) {
+      this.$store
+        .dispatch("firm/removeOne", { firmID: params })
+        .then(() => {})
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    open(func, data, message) {
+      this.$confirm(message, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          func(data);
+          this.loadData();
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    }
+  },
+  mounted() {
+    this.loadData();
+  },
+  components: {
+    firmEdit,
+    firmSearch
+  }
+};
 </script>
--- src/views/staff/List.vue
+++ src/views/staff/List.vue
@@ -3,6 +3,7 @@
     <el-row :gutter="20">
       <el-col :xs="11" :sm="10" :md="9" :lg="8" :xl="8">
         <el-tree
+          v-loading="loading"
           :data="treeData"
           :props="treeProps"
           :highlight-current="true"
@@ -11,52 +12,56 @@
         ></el-tree>
       </el-col>
       <el-col :xs="13" :sm="14" :md="15" :lg="16" :xl="16">
-        <staff-form :curNode="curNode" :staffAddVisible="staffAddVisible" style="width: 100%"/>
+        <staff-form :curNode="curNode" :staffAddVisible="staffAddVisible" style="width: 100%" />
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-    import staffForm from "./Edit";
+import staffForm from "./Edit";
 
-    export default {
-        data() {
-            return {
-                staffAddVisible: true,
-                curNode: null,
-                treeData: [],
-                treeProps: {
-                    label: "deptName",
-                    children: "children"
-                }
-            };
-        },
-        methods: {
-            /*获取部门树*/
-            loadTreeData() {
-                this.$store
-                    .dispatch("dept/getList", {
-                        filters: {}
-                    })
-                    .then(data => {
-                        this.treeData = data;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            },
-            /*点击部门树时调用*/
-            handleNodeClick(data) {
-                this.staffAddVisible = false;
-                this.curNode = data;
-            }
-        },
-        mounted() {
-            this.loadTreeData();
-        },
-        components: {
-            staffForm
-        }
+export default {
+  data() {
+    return {
+      staffAddVisible: true,
+      loading: true,
+      curNode: null,
+      treeData: [],
+      treeProps: {
+        label: "deptName",
+        children: "children"
+      }
     };
+  },
+  methods: {
+    /*获取部门树*/
+    loadTreeData() {
+      this.$store
+        .dispatch("dept/getList", {
+          filters: {}
+        })
+        .then(data => {
+          if (data) {
+            this.treeData = data;
+            this.loading = false;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    /*点击部门树时调用*/
+    handleNodeClick(data) {
+      this.staffAddVisible = false;
+      this.curNode = data;
+    }
+  },
+  mounted() {
+    this.loadTreeData();
+  },
+  components: {
+    staffForm
+  }
+};
 </script>
