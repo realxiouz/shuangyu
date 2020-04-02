98dbf527d696c92d7ccecc0b8e1b42dacef93dd8
李思韩 <1915844250@qq.com>
2020-04-02 16:45:50 +0800

    样式修改

--- src/views/fundAccount/Edit.vue
+++ src/views/fundAccount/Edit.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <el-form :model="formData" label-width="110px" size="mini">
-      <input type="hidden" v-model="formData.fundAccountId"/>
-      <el-form-item label="资金账号平台">
-        <el-input v-model="formData.platform" placeholder="请输入资金账号平台.."></el-input>
-      </el-form-item>
+      <input type="hidden" v-model="formData.fundAccountId" />
       <el-form-item label="资金账号">
         <el-input v-model="formData.fundAccount" placeholder="请输入资金账号.."></el-input>
+      </el-form-item>
+      <el-form-item label="账号平台">
+        <el-input v-model="formData.platform" placeholder="请输入资金账号平台.."></el-input>
       </el-form-item>
       <el-form-item label="初始余额">
         <el-input v-model="formData.initBalance" placeholder="请输入初始余额.."></el-input>
@@ -23,41 +23,41 @@
 </template>
 
 <script>
-    export default {
-        props: ["curNode", "update"],
-        data() {
-            return {
-                formData: {}
-            };
-        },
-        methods: {
-            /*表单默认加载数据*/
-            defaultFormData() {
-                return {
-                    fundAccountId: '',
-                    platform: '',
-                    fundAccount: '',
-                    initBalance: 0,
-                    balance: 0
-                };
-            },
-            /*清除表单*/
-            clearForm() {
-                this.formData = this.defaultFormData();
-            },
-            /*对提交的数据进行类型格式*/
-            handleConfirm(){
-                this.$emit('onSave',this.formData);
-            },
-            initFormData(){
-                this.clearForm();
-                if (this.update){
-                    this.formData = this.curNode;
-                }
-            }
-        },
-        created() {
-            this.initFormData();
-        }
+export default {
+  props: ["curNode", "update"],
+  data() {
+    return {
+      formData: {}
     };
+  },
+  methods: {
+    /*表单默认加载数据*/
+    defaultFormData() {
+      return {
+        fundAccountId: "",
+        platform: "",
+        fundAccount: "",
+        initBalance: 0,
+        balance: 0
+      };
+    },
+    /*清除表单*/
+    clearForm() {
+      this.formData = this.defaultFormData();
+    },
+    /*对提交的数据进行类型格式*/
+    handleConfirm() {
+      this.$emit("onSave", this.formData);
+    },
+    initFormData() {
+      this.clearForm();
+      if (this.update) {
+        this.formData = this.curNode;
+      }
+    }
+  },
+  created() {
+    this.initFormData();
+  }
+};
 </script>
