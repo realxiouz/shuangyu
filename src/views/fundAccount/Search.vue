7390ff3030006284b4164c357f39082c5012e646
李思韩 <1915844250@qq.com>
2020-04-02 10:04:23 +0800

    样式修改

--- src/views/fundAccount/Search.vue
+++ src/views/fundAccount/Search.vue
@@ -1,10 +1,10 @@
 <template>
   <el-row type="flex" justify="space-between" align="bottom">
     <el-col :xs="16" :sm="18" :md="18" :lg="20" :xl="20">
-      <el-form label-width="80px" size="mini">
+      <el-form :model="formData" label-width="80px" size="mini">
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
           <el-form-item label="资金账号">
-            <el-input v-model="keyword" placeholder="请输入资金账号.." size=""></el-input>
+            <el-input v-model="formData.keyword" placeholder="请输入资金账号.." size></el-input>
           </el-form-item>
         </el-col>
       </el-form>
@@ -16,7 +16,7 @@
         class="filter-item"
         type="primary"
         size="mini"
-        @click="$emit('onSearch', keyword)"
+        @click="$emit('onSearch', formData)"
       >查询</el-button>
       <el-button type="text" size="mini" @click="handleMore">
         更多
@@ -26,27 +26,29 @@
   </el-row>
 </template>
 <script>
-    export default {
-        name: "fundAccountSearch",
-        data() {
-            return {
-                more: false,
-                keyword: null
-            };
-        },
-        computed: {
-            switchIcon() {
-                if (!this.more) {
-                    return "el-icon-arrow-down el-icon--right";
-                } else {
-                    return "el-icon-arrow-up el-icon--right";
-                }
-            }
-        },
-        methods: {
-            handleMore() {
-                this.more = !this.more;
-            }
-        }
+export default {
+  name: "fundAccountSearch",
+  data() {
+    return {
+      more: false,
+      formData: {
+        keyword: null
+      }
     };
+  },
+  computed: {
+    switchIcon() {
+      if (!this.more) {
+        return "el-icon-arrow-down el-icon--right";
+      } else {
+        return "el-icon-arrow-up el-icon--right";
+      }
+    }
+  },
+  methods: {
+    handleMore() {
+      this.more = !this.more;
+    }
+  }
+};
 </script>
