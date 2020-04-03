69a952fcb6377497ad6163fbed8f15983fbfa340
李思韩 <1915844250@qq.com>
2020-04-03 10:17:23 +0800

    样式修改

--- src/views/orderInfo/Search.vue
+++ src/views/orderInfo/Search.vue
@@ -3,21 +3,125 @@
     <el-col :xs="16" :sm="18" :md="18" :lg="20" :xl="20">
       <el-form :model="formData" label-width="110px" size="mini">
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="任务编号:">
+          <el-form-item label="订单号:">
             <el-input
-              v-model="formData.taskNo"
               @keyup.enter.native="$emit('onSearch', formData)"
-              placeholder="请输入任务编号搜索..."
+              v-model="formData.orderNo"
+              style="width: 100%"
             ></el-input>
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="任务名称:">
+          <el-form-item label="票号:">
             <el-input
-              v-model="formData.taskName"
               @keyup.enter.native="$emit('onSearch', formData)"
-              placeholder="请输入任务名称搜索..."
+              v-model="formData.ticketNo"
+              style="width: 100%"
             ></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item label="乘机人姓名:">
+            <el-input
+              @keyup.enter.native="$emit('onSearch', formData)"
+              v-model="formData.name"
+              style="width: 100%"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="乘机人证件号:">
+            <el-input
+              @keyup.enter.native="$emit('onSearch', formData)"
+              v-model="formData.cardNo"
+              style="width: 100%"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="PNR:">
+            <el-input
+              @keyup.enter.native="$emit('onSearch', formData)"
+              v-model="formData.pnr"
+              style="width: 100%"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="订单状态:">
+            <el-select clearable v-model="formData.status" placeholder="全部" style="width: 100%">
+              <el-option label="出票中" value="3"></el-option>
+              <el-option label="出票完成" value="4"></el-option>
+              <el-option label="退票完成" value="13"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="出发日期:">
+            <el-col>
+              <el-date-picker
+                type="date"
+                placeholder="选择日期"
+                v-model="formData.flightDate"
+                style="width: 100%;"
+                format="yyyy-MM-dd"
+                value-format="yyyy-MM-dd"
+              ></el-date-picker>
+            </el-col>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="舱位:">
+            <el-input
+              @keyup.enter.native="$emit('onSearch', formData)"
+              v-model="formData.cabin"
+              style="width: 100%"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="航班号:">
+            <el-input
+              @keyup.enter.native="$emit('onSearch', formData)"
+              v-model="formData.flightCode"
+              style="width: 100%"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="订单类型:">
+            <el-select clearable  v-model="formData.orderType" placeholder="全部" style="width: 100%">
+              <el-option label="出票完成" value="10"></el-option>
+              <el-option label="改签完成" value="30"></el-option>
+              <el-option label="二次改签" value="31"></el-option>
+              <el-option label="退票完成" value="20"></el-option>
+              <el-option label="二次退票" value="21"></el-option>
+              <el-option label="退差" value="22"></el-option>
+              <el-option label="退改" value="23"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="18" :md="14" :lg="10" :xl="6">
+          <el-form-item v-show="more" label="航程类型:">
+            <el-radio-group v-model="formData.voyageType" style="width: 100%">
+              <el-radio label="0">单程</el-radio>
+              <el-radio label="1">往返</el-radio>
+              <el-radio label="2">连程</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item v-show="more" label="订单日期:">
+            <el-col>
+              <el-date-picker
+                type="date"
+                placeholder="选择日期"
+                v-model="formData.createTime"
+                style="width: 100%;"
+                format="yyyy-MM-dd"
+                value-format="yyyy-MM-dd"
+              ></el-date-picker>
+            </el-col>
           </el-form-item>
         </el-col>
       </el-form>
@@ -40,13 +144,23 @@
 
 <script>
 export default {
-  name: "userSearch",
+  name: "orderSearch",
   data() {
     return {
       more: false,
       formData: {
-        taskNo: "",
-        taskName: ""
+        voyageType: "", //航程类型
+        orderType: "", //订单来源
+        flightCode: "", //航班号
+        flightDate: "", //出发日期
+        cabin: "", //舱位
+        status: "", //订单状态
+        pnr: "", //PNR
+        ticketNo: "", //票号
+        orderNo: "", //订单号
+        name: "", //乘机人姓名
+        cardNo: "", //乘机人证件号
+        createTime: ""
       }
     };
   },
