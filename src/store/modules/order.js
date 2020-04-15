c344ae6fbdec4bdad3b2af717d517150e4fe86d3
李思韩 <1915844250@qq.com>
2020-04-15 11:35:05 +0800

    Default Changelist

--- src/store/modules/order.js
+++ src/store/modules/order.js
@@ -1,4 +1,4 @@
-import { getOrderTree, getList, getOne, removeOne, save, getTimeCount, getCount, getTotal, getOrderDetail, getOrderMinPrice, getOrderFlight, getFlightPrice, placeAnOrder, openPay } from "@/api/order";
+import { unLockOrder,lockOrder,getOrderTree, getList, getOne, removeOne, save, getTimeCount, getCount, getTotal, getOrderDetail, getOrderMinPrice, getOrderFlight, getFlightPrice, placeAnOrder, openPay } from "@/api/order";
 import { getToken } from "@/utils/auth";
 
 
@@ -189,6 +189,34 @@ const actions = {
   getOrderTree({ commit }, params) {
     return new Promise((resolve, reject) => {
       getOrderTree(params)
+        .then(response => {
+          const { data } = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  
+  lockOrder({ commit }, params) {
+    const  orderNo  = params;
+    return new Promise((resolve, reject) => {
+      lockOrder(orderNo)
+        .then(response => {
+          const { data } = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  
+  unLockOrder({ commit }, params) {
+    const  orderNo  = params;
+    return new Promise((resolve, reject) => {
+      unLockOrder(orderNo)
         .then(response => {
           const { data } = response;
           resolve(data);
