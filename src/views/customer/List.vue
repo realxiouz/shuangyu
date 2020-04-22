92019ebc859a3cac1086283b7101ccf5b83a6bd8
sunlinfan <linfansun@163.com>
2020-04-22 10:27:43 +0800

    配置管理

--- src/views/customer/List.vue
+++ src/views/customer/List.vue
@@ -23,11 +23,11 @@
         <el-table-column prop="remark" label="备注" align="center"></el-table-column>
         <el-table-column label="操作" fixed="right">
           <template slot-scope="scope">
+<!--            <span v-show="scope.row.openId && '' != scope.row.openId">-->
+              <el-button type="info" size="mini" @click="handleSupplement(scope.$index, scope.row)">配置管理</el-button>
+<!--            </span>-->
             <el-button type="primary" size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
             <el-button type="danger" size="mini" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-            <span>
-              <el-button type="info" size="mini" @click="handleSupplement(scope.row)">配置管理</el-button>
-            </span>
           </template>
         </el-table-column>
       </el-table>
