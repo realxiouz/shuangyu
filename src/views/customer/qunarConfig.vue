3d2eca49f7e3ee5514d9d8405f89c0e7513022fe
sunlinfan <linfansun@163.com>
2020-04-25 13:41:45 +0800

    remark

--- src/views/customer/qunarConfig.vue
+++ src/views/customer/qunarConfig.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="bigBox">
+    <div id="goBack" @click="goBack">
+      <el-page-header></el-page-header>
+    </div>
     <el-card class="contentBox">
       <div slot="header">
         <span>去哪儿订单通知接口</span>
@@ -399,6 +402,15 @@ http://123.123.123.1:9000</span>
                         console.error(err);
                     });
             },
+            //跳转回列表页面
+            goBack() {
+                if (this.$router.history.length <= 1) {
+                    this.$router.push({path: '/home'});
+                    return false;
+                } else {
+                    this.$router.go(-1);
+                }
+            }
         },
         created() {
             this.domain = this.$route.query.domain;
@@ -424,3 +436,9 @@ http://123.123.123.1:9000</span>
         }
     };
 </script>
+<style>
+  #goBack {
+    padding-left: 10px;
+    padding-top: 10px;
+  }
+</style>
