5caa95bb7ecce14de5b808abbcf68f0e02541d2f
nuter <13170600305@163.com>
2020-04-28 20:05:37 +0800

    更新命名。

--- src/views/dict/List.vue
+++ src/views/dict/List.vue
@@ -10,9 +10,9 @@
           :data="treeData"
           :default-expanded-keys="curLine"
           :props="treeProps"
+          :expand-on-click-node="false"
           :highlight-current="true"
-          @node-click="handleNodeClick"
-        >
+          @node-click="handleNodeClick">
           <span class="tree-node" slot-scope="{ node, data }">
             <span>{{ node.data.categoryName }}</span>
             <span>
@@ -24,7 +24,7 @@
         </el-tree>
       </el-col>
       <el-col :xs="13" :sm="14" :md="15" :lg="16" :xl="18">
-        <dict-edit :dictVisible="dictVisible" :curNode="curNode" />
+        <dict-edit :dictVisible="dictVisible" :curNode="curNode"/>
       </el-col>
     </el-row>
     <el-dialog
@@ -35,23 +35,21 @@
       :close-on-click-modal="false"
     >
       <el-form :model="formData" label-width="90px" size="mini">
-        <input type="hidden" v-model="formData.categoryId" />
+        <input type="hidden" v-model="formData.categoryId"/>
+        <el-form-item label="类别类型">
+          <el-input type="text" v-model="formData.categoryType" :disabled="typeEditAbel || update" placeholder="请输入类别类型.."></el-input>
+        </el-form-item>
         <el-form-item label="类别编码">
-          <el-input
-            type="text"
-            v-model="formData.categoryCode"
-            @input="toUpperCase"
-            :disabled="update"
-          ></el-input>
+          <el-input type="text" v-model="formData.categoryCode" @input="toUpperCase" :disabled="update" placeholder="请输入类别编码.."></el-input>
         </el-form-item>
         <el-form-item label="类别名称">
-          <el-input type="text" v-model="formData.categoryName"></el-input>
+          <el-input type="text" v-model="formData.categoryName" placeholder="请输入类别名称.."></el-input>
         </el-form-item>
         <el-form-item label="排序">
           <el-input type="text" v-model="formData.sort"></el-input>
         </el-form-item>
         <el-form-item label="备注">
-          <el-input type="textarea" :rows="3" v-model="formData.remark"></el-input>
+          <el-input type="textarea" :rows="3" v-model="formData.remark" placeholder="请输入备注.."></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -63,208 +61,212 @@
 </template>
 
 <script>
-import dictEdit from "./Edit";
+    import dictEdit from "./Edit";
 
-export default {
-  data() {
-    return {
-      addFlag: true,
-      loading: true,
-      dialogVisible: false,
-      /*是否选择类别节点，没有选择则不可编辑*/
-      dictVisible: true,
-      update: false,
-      /*判断是否添加的是根类别节点*/
-      root: false,
-      /*类别树数据列表*/
-      treeData: [],
-      /*在添加完一条导航数据后用于展开该条类别分支*/
-      curLine: [],
-      /*需要进行编辑的类别节点*/
-      curNode: {},
-      /*类别节点添加编辑表单*/
-      formData: {},
-      treeProps: {
-        children: "children",
-        hasChildren: "xxx"
-      }
+    export default {
+        data() {
+            return {
+                addFlag: true,
+                loading: true,
+                dialogVisible: false,
+                /*是否选择类别节点，没有选择则不可编辑*/
+                dictVisible: true,
+                update: false,
+                /*判断是否添加的是根类别节点*/
+                root: false,
+                /*类别树数据列表*/
+                treeData: [],
+                /*在添加完一条导航数据后用于展开该条类别分支*/
+                curLine: [],
+                /*需要进行编辑的类别节点*/
+                curNode: {},
+                /*类别节点添加编辑表单*/
+                formData: {},
+                typeEditAbel: false,
+                treeProps: {
+                    children: "children",
+                    hasChildren: "xxx"
+                }
+            };
+        },
+        methods: {
+            /*初始化类别添加表单*/
+            defaultFormData() {
+                return {
+                    categoryId: "",
+                    categoryCode: "",
+                    categoryType: null,
+                    categoryName: "",
+                    icon: "",
+                    title: "",
+                    sort: "",
+                    remark: ""
+                };
+            },
+            /*加载类别树*/
+            loadData() {
+                this.$store
+                    .dispatch("category/getTreeList", {filter: {}})
+                    .then(data => {
+                        if (data) {
+                            this.treeData = data.data;
+                        }
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        console.log(error);
+                    });
+            },
+            /*点击添加顶级企业信息*/
+            rootAdd() {
+                this.addFlag = true;
+                this.typeEditAbel = false;
+                this.formData = this.defaultFormData();
+                this.formData.pid = null;
+                this.formData.level = 0;
+
+                //判断添加的导航是否是顶级导航
+                this.root = true;
+                this.dialogVisible = true;
+                this.update = false;
+            },
+            /*点击添加节点企业信息*/
+            nodeAdd(idx, node) {
+                this.addFlag = true;
+                this.typeEditAbel = true;
+                this.formData = this.defaultFormData();
+                this.formData.pid = node.categoryId;
+                this.formData.level = node.level + 1;
+                this.formData.categoryType = node.categoryType;
+
+                //添加的导航菜单不是顶级菜单
+                this.root = false;
+                this.dialogVisible = true;
+                this.update = false;
+            },
+            /*对导航节点进行存储*/
+            handleSave() {
+                this.dialogVisible = false;
+                this.curLine = [];
+
+                if (this.formData.categoryId != "") {
+                    this.$store
+                        .dispatch("category/updateOne", this.formData)
+                        .then(() => {
+                            this.loadData();
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        });
+                } else {
+                    this.$store
+                        .dispatch("category/addOne", this.formData)
+                        .then(data => {
+                            this.curLine.push(data.data);
+                            this.loadData();
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        });
+                }
+                this.clearForm();
+            },
+            /*点击移除导航节点*/
+            removeNode(data, node) {
+                this.curLine = [];
+                if (null != node.pid) {
+                    this.curLine.push(node.pid);
+                } else {
+                    this.curLine = [];
+                }
+                this.open(
+                    this.remove,
+                    node.categoryId,
+                    "此操作将删除该类别及子类别的信息, 是否继续?"
+                );
+            },
+            /*移除类别节点*/
+            remove(params) {
+                this.$store
+                    .dispatch("category/removeOne", {categoryId: params})
+                    .then(() => {
+                        this.loadData();
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            /*点击取消按钮*/
+            handleCancel() {
+                this.clearForm();
+                this.dialogVisible = false;
+            },
+            /*选择类别节点后对该节点进行编辑*/
+            handleNodeClick(data, node) {
+                if (!node.data.hasChildren) {
+                    this.curNode = node.data;
+                    this.dictVisible = false;
+                } else {
+                    this.dictVisible = true;
+                }
+            },
+            handleEdit(data, node) {
+                this.addFlag = false;
+                this.formData = node;
+                this.dialogVisible = true;
+                this.update = true;
+
+                this.curLine = [];
+                if (null != node.pid) {
+                    this.curLine.push(node.pid);
+                } else {
+                    this.curLine = [];
+                }
+            },
+            clearForm() {
+                this.formData = this.defaultFormData();
+            },
+            open(func, data, message) {
+                this.$confirm(message, "提示", {
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    type: "warning"
+                })
+                    .then(() => {
+                        func(data);
+                        this.$message({
+                            type: "success",
+                            message: "删除成功!"
+                        });
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: "info",
+                            message: "已取消删除"
+                        });
+                    });
+            },
+            toUpperCase() {
+                this.formData.categoryCode = this.formData.categoryCode.toUpperCase();
+            }
+        },
+        created() {
+            this.loadData();
+        },
+        components: {
+            dictEdit
+        }
     };
-  },
-  methods: {
-    /*初始化类别添加表单*/
-    defaultFormData() {
-      return {
-        categoryId: "",
-        categoryCode: "",
-        categoryType: 0,
-          categoryName: "",
-        icon: "",
-        title: "",
-        sort: "",
-        remark: ""
-      };
-    },
-    /*加载类别树*/
-    loadData() {
-      this.$store
-        .dispatch("category/getTreeList", { filter: { categoryType: 0 } })
-        .then(data => {
-          if (data) {
-            this.treeData = data.data;
-          }
-          this.loading = false;
-        })
-        .catch(error => {
-          this.loading = false;
-          console.log(error);
-        });
-    },
-    /*点击添加顶级企业信息*/
-    rootAdd() {
-      this.addFlag = true;
-      this.formData = this.defaultFormData();
-      this.formData.pid = null;
-      this.formData.level = 0;
-
-      //判断添加的导航是否是顶级导航
-      this.root = true;
-      this.dialogVisible = true;
-      this.update = false;
-    },
-    /*点击添加节点企业信息*/
-    nodeAdd(idx, node) {
-      this.addFlag = true;
-      this.formData = this.defaultFormData();
-      this.formData.pid = node.categoryId;
-      this.formData.level = node.level + 1;
-
-      //添加的导航菜单不是顶级菜单
-      this.root = false;
-      this.dialogVisible = true;
-      this.update = false;
-    },
-    /*对导航节点进行存储*/
-    handleSave() {
-      this.dialogVisible = false;
-      this.curLine = [];
-
-      if (this.formData.categoryId != "") {
-        this.$store
-          .dispatch("category/updateOne", this.formData)
-          .then(() => {
-            this.loadData();
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      } else {
-        this.$store
-          .dispatch("category/addOne", this.formData)
-          .then(data => {
-            this.curLine.push(data.data);
-            this.loadData();
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
-      this.clearForm();
-    },
-    /*点击移除导航节点*/
-    removeNode(data, node) {
-      this.curLine = [];
-      if (null != node.pid) {
-        this.curLine.push(node.pid);
-      } else {
-        this.curLine = [];
-      }
-      this.open(
-        this.remove,
-        node.categoryId,
-        "此操作将删除该类别及子类别的信息, 是否继续?"
-      );
-    },
-    /*移除类别节点*/
-    remove(params) {
-      this.$store
-        .dispatch("category/removeOne", { categoryId: params })
-        .then(() => {
-          this.loadData();
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    /*点击取消按钮*/
-    handleCancel() {
-      this.clearForm();
-      this.dialogVisible = false;
-    },
-    /*选择类别节点后对该节点进行编辑*/
-    handleNodeClick(data, node) {
-      if (!node.data.hasChildren) {
-        this.curNode = node.data;
-        this.dictVisible = false;
-      } else {
-        this.dictVisible = true;
-      }
-    },
-    handleEdit(data, node) {
-      this.addFlag = false;
-      this.formData = node;
-      this.dialogVisible = true;
-      this.update = true;
-
-      this.curLine = [];
-      if (null != node.pid) {
-        this.curLine.push(node.pid);
-      } else {
-        this.curLine = [];
-      }
-    },
-    clearForm() {
-      this.formData = this.defaultFormData();
-    },
-    open(func, data, message) {
-      this.$confirm(message, "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      })
-        .then(() => {
-          func(data);
-          this.$message({
-            type: "success",
-            message: "删除成功!"
-          });
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消删除"
-          });
-        });
-    },
-    toUpperCase() {
-      this.formData.categoryCode = this.formData.categoryCode.toUpperCase();
-    }
-  },
-  created() {
-    this.loadData();
-  },
-  components: {
-    dictEdit
-  }
-};
 </script>
 
 <style scoped>
-.tree-node {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 14px;
-  padding-right: 8px;
-}
+  .tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
 </style>
