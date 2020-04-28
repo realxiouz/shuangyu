1ff28f87a756d93736930fe0d099340f02f10b7b
nuter <13170600305@163.com>
2020-04-28 20:03:14 +0800

    实现商品类别的选择。

--- src/views/brand/Edit.vue
+++ src/views/brand/Edit.vue
@@ -8,7 +8,14 @@
         <el-input v-model="formData.brandName" placeholder="请输入品牌编码.."></el-input>
       </el-form-item>
       <el-form-item label="商品类目：">
-        <el-input v-model="formData.categoryCode" placeholder="请输入商品类目.."></el-input>
+        <el-cascader
+          v-model="formData.categoryCode"
+          style="width: 100%;"
+          placeholder="可通过科目名称搜索.."
+          :options="categoryList"
+          :props="{ label: 'categoryName', value: 'categoryCode' }"
+          @change="selectedCategory">
+        </el-cascader>
       </el-form-item>
       <el-form-item label="品牌故事：">
         <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" v-model="formData.brandStory" placeholder="请输入品牌故事.."></el-input>
@@ -29,7 +36,10 @@
         props: ["curNode", "update"],
         data() {
             return {
-                formData: {}
+                formData: {},
+                categoryList: [],
+                //用于记录和查找所选中的商品类别
+                tempCategoryList: []
             };
         },
         methods: {
@@ -55,6 +65,15 @@
                     description: ''
                 };
             },
+            loadCategory(){
+                this.$store.dispatch("category/getTreeList", {filter: {categoryType: 9}})
+                    .then(data => {
+                        this.categoryList = this.generateTreeData(data.data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
             /*清除表单*/
             clearForm() {
                 this.formData = this.defaultFormData();
@@ -63,12 +82,39 @@
             handleConfirm() {
                 this.$emit("onSave", this.formData);
             },
+            selectedCategory(selected){
+                //选取最后的节点
+                const code = selected[selected.length - 1];
+                this.formData.categoryCode = code;
+                //找到所选择的对象
+                this.tempCategoryList.forEach(item => {
+                    if (code == item.categoryCode){
+                        this.formData.categoryName = item.categoryName;
+                        this.formData.categoryPath = item.path;
+                    }
+                })
+            },
             initFormData() {
                 this.clearForm();
+                this.loadCategory();
                 if (this.update) {
                     Object.assign(this.formData,this.curNode);
                 }
-            }
+            },
+            generateTreeData(data) {
+                // 循环遍历json数据
+                for (let i = 0; i < data.length; i++) {
+                    this.tempCategoryList.push(data[i]);
+                    if (data[i].children.length < 1) {
+                        // children若为空数组，则将children设为undefined
+                        data[i].children = undefined;
+                    } else {
+                        // children若不为空数组，则继续 递归调用 本方法
+                        this.generateTreeData(data[i].children);
+                    }
+                }
+                return data;
+            },
         },
         created() {
             this.initFormData();
