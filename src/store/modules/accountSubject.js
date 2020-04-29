86db49913e059d7d6518ab16057aa2834b944f49
nuter <13170600305@163.com>
2020-04-29 10:17:44 +0800

    getList();通过过滤条件查询符合条件的数据。

--- src/store/modules/accountSubject.js
+++ src/store/modules/accountSubject.js
@@ -1,4 +1,4 @@
-import {getTreeList, getOne, getPageList, getTotal, removeOne, save, getSelectingList} from "@/api/accountSubject";
+import {getList,getTreeList, getOne, getPageList, getTotal, removeOne, save, getSelectingList} from "@/api/accountSubject";
 import {getToken} from "@/utils/auth";
 
 
@@ -37,6 +37,19 @@ const actions = {
     const {filters} = params;
     return new Promise((resolve, reject) => {
       getTreeList(filters)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getList({commit}, params) {
+    const {filters} = params;
+    return new Promise((resolve, reject) => {
+      getList(filters)
         .then(response => {
           const {data} = response;
           resolve(data);
