e1d6040ecdf3e6e5d871978318cb76e2f5810dda
sunlinfan <linfansun@163.com>
2020-05-02 17:05:13 +0800

    产品

--- src/views/product/Edit.vue
+++ src/views/product/Edit.vue
@@ -15,7 +15,7 @@
       </el-row>
       <el-row :gutter="10">
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="商品类目">
+          <el-form-item label="商品类目" prop="categoryCode">
             <el-cascader
               v-model="formData.categoryCode"
               style="width: 100%;"
@@ -128,6 +128,13 @@
       </el-row>
       <el-row :gutter="10">
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+          <el-form-item label="最小订单量" prop="miniOrderQuantity">
+            <el-input v-model="formData.miniOrderQuantity"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="10">
+        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
           <el-form-item label="规格" prop="specification">
             <el-input v-model="formData.specification"></el-input>
           </el-form-item>
@@ -138,13 +145,7 @@
           </el-form-item>
         </el-col>
       </el-row>
-      <el-row :gutter="10">
-        <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-          <el-form-item label="最小订单量" prop="miniOrderQuantity">
-            <el-input v-model="formData.miniOrderQuantity"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
+
       <el-row :gutter="10">
         <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
           <el-form-item label="毛利率" prop="grossMargin">
@@ -179,22 +180,80 @@
       </el-row>
     </el-form>
     <el-table
-      :data="tableData"
+      :data="dataList"
       border
       style="width: 100%">
       <el-table-column
-        prop="date"
-        label="日期"
+        prop="warehouseCode"
+        label="编号"
         width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.warehouseCode"></el-input>
+        </template>
       </el-table-column>
       <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
+        prop="warehouseName"
+        label="名称"
+        width="200">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.warehouseName"></el-input>
+        </template>
       </el-table-column>
       <el-table-column
-        prop="address"
-        label="地址">
+        prop="categoryName"
+        label="商品分类"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.categoryName"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="quantity"
+        label="数量"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.quantity"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="maxStockQuantity"
+        label="库存上限"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.maxStockQuantity"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="minStockQuantity"
+        label="库存下限"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.minStockQuantity"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="unit"
+        label="计量单位"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.unit"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="cost"
+        label="成本"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.cost"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="零售价"
+        width="180">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.price"></el-input>
+        </template>
       </el-table-column>
     </el-table>
     <div slot="footer" style="text-align:center;">
@@ -204,198 +263,220 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'edit',
-    data() {
-      return {
-        formData: {
-          properties: []
-        },
-        propertyList: [],
-        categoryList: [],
-        brandList: [],
-        dataList: [],
-        rules: {
-          productCode: [
-            {required: true, message: "请输入商品编码", trigger: "blur"},
-            {
-              min: 1,
-              max: 20,
-              message: "长度在 1到 20 个字符"
-            }
-          ],
-          productName: [
-            {required: true, message: "请输入商品名称", trigger: "blur"},
-            {
-              min: 1,
-              max: 20,
-              message: "长度在 1到20 个字符"
-            }
-          ],
-          brandCode: [
-            {required: true, message: "请选择一个品牌", trigger: "blur"},
-            {
-              min: 1,
-              max: 20,
-              message: "长度在 1到20 个字符"
-            }
-          ]
-        }
-      }
-    },
-    methods: {
-      calcDescartes(array) {
-        if (array.length < 2) return array[0] || [];
-        return [].reduce.call(array, function (col, set) {
-          let res = [];
-          col.forEach(function (c) {
-            set.forEach(function (s) {
-              let t = [].concat(Array.isArray(c) ? c : [c]);
-              t.push(s);
-              res.push(t);
-            })
-          });
-          return res;
-        });
-      },
-      handleSku(value, item, item3) {
-        console.log(value);
-        console.log(item);
-        console.log(item3);
-      },
-      handleSave() {
-        this.$refs['form'].validate((valid) => {
-          if (valid) {
-            this.$store
-              .dispatch("product/save", this.formData)
-              .then(() => {
-              })
-              .catch(error => {
-                console.log(error);
-              });
-            this.$message({
-              type: "success",
-              message: "保存成功！"
-            });
-            this.goBack();
-          }
-        });
-      },
-      handleCancel() {
-        this.goBack();
-      },
-      handleGetOne(id) {
-        this.$store
-          .dispatch("product/getOne", {productId: id})
-          .then(data => {
-            this.formData = data;
-            let param = {};
-            param.categoryCode = this.formData.categoryCode;
-            this.loadpropertyList(param);
-            this.dialogVisible = true;
-          }).catch(error => {
-          console.log(error);
-        });
-      },
-      loadpropertyList(searchForm) {
-        this.$store
-          .dispatch("productProperty/getList", {
-            filter: searchForm
-          })
-          .then(data => {
-            if (data) {
-              this.propertyList = data;
-              let properties = this.formData.properties;
-              this.formData.properties = [];
-              for (let i = 0, len = data.length; i < len; i++) {
-                if (data[i].valueType > 6) {
-                  this.formData.properties.push({
-                    label: data[i].propertyLabel,
-                    code: data[i].propertyCode,
-                    value: this.getValue(data[i].propertyCode, properties, [])
-                  });
-                } else {
-                  this.formData.properties.push({
-                    label: data[i].propertyLabel,
-                    code: data[i].propertyCode,
-                    value: this.getValue(data[i].propertyCode, properties, '')
-                  });
+    export default {
+        name: 'edit',
+        data() {
+            return {
+                formData: {
+                    properties: []
+                },
+                propertyList: [],
+                categoryList: [],
+                brandList: [],
+                dataList: [],
+                rules: {
+                    productCode: [
+                        {required: true, message: "请输入商品编码", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到 20 个字符"
+                        }
+                    ],
+                    productName: [
+                        {required: true, message: "请输入商品名称", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到20 个字符"
+                        }
+                    ],
+                    brandCode: [
+                        {required: true, message: "请选择一个品牌", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到20 个字符"
+                        }
+                    ],
+                    categoryCode: [
+                        {required: true, message: "请选择商品类目", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到20 个字符"
+                        }
+                    ],
+                    unit: [{required: true, message: "请输入计量单位", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到20 个字符"
+                        }],
+                    miniOrderQuantity: [
+                        {required: true, message: "请输入最小订单量", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到20 个字符"
+                        }
+                    ]
                 }
-              }
             }
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      },
-      getValue(code, properties, defaultValue) {
-        for (let i = 0, len = properties.length; i < len; i++) {
-          if (properties[i].code == code) {
-            return properties[i].value;
-          }
-        }
-        return defaultValue;
-      },
-      loadBrand() {
-        this.$store.dispatch("brand/getList", {filters: {}})
-          .then(data => {
-            this.brandList = data;
-          }).catch(error => {
-          console.log(error);
-        });
-      },
-      loadTreeData() {
-        this.$store
-          .dispatch("category/getTreeList", {filter: {categoryType: 9}})
-          .then(data => {
-            if (data) {
-              this.categoryList = this.getTreeData(data.data);
+        },
+        methods: {
+            calcDescartes(array) {
+                if (array.length < 2) return array[0] || [];
+                return [].reduce.call(array, function (col, set) {
+                    let res = [];
+                    col.forEach(function (c) {
+                        set.forEach(function (s) {
+                            let t = [].concat(Array.isArray(c) ? c : [c]);
+                            t.push(s);
+                            res.push(t);
+                        })
+                    });
+                    return res;
+                });
+            },
+            handleSku(value, item, item3) {
+                console.log(value);
+                console.log(item);
+                console.log(item3);
+            },
+            handleSave() {
+                this.$refs['form'].validate((valid) => {
+                    if (valid) {
+                        this.$store
+                            .dispatch("product/save", this.formData)
+                            .then(() => {
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            });
+                        this.$message({
+                            type: "success",
+                            message: "保存成功！"
+                        });
+                        this.goBack();
+                    }
+                });
+            },
+            handleCancel() {
+                this.goBack();
+            },
+            handleGetOne(id) {
+                this.$store
+                    .dispatch("product/getOne", {productId: id})
+                    .then(data => {
+                        this.formData = data;
+                        let param = {};
+                        param.categoryCode = this.formData.categoryCode;
+                        this.loadpropertyList(param);
+                        this.dialogVisible = true;
+                    }).catch(error => {
+                    console.log(error);
+                });
+            },
+            loadpropertyList(searchForm) {
+                this.$store
+                    .dispatch("productProperty/getList", {
+                        filter: searchForm
+                    })
+                    .then(data => {
+                        if (data) {
+                            this.propertyList = data;
+                            let properties = this.formData.properties;
+                            this.formData.properties = [];
+                            for (let i = 0, len = data.length; i < len; i++) {
+                                if (data[i].valueType > 6) {
+                                    this.formData.properties.push({
+                                        label: data[i].propertyLabel,
+                                        code: data[i].propertyCode,
+                                        value: this.getValue(data[i].propertyCode, properties, [])
+                                    });
+                                } else {
+                                    this.formData.properties.push({
+                                        label: data[i].propertyLabel,
+                                        code: data[i].propertyCode,
+                                        value: this.getValue(data[i].propertyCode, properties, '')
+                                    });
+                                }
+                            }
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            getValue(code, properties, defaultValue) {
+                for (let i = 0, len = properties.length; i < len; i++) {
+                    if (properties[i].code == code) {
+                        return properties[i].value;
+                    }
+                }
+                return defaultValue;
+            },
+            loadBrand() {
+                this.$store.dispatch("brand/getList", {filters: {}})
+                    .then(data => {
+                        this.brandList = data;
+                    }).catch(error => {
+                    console.log(error);
+                });
+            },
+            loadTreeData() {
+                this.$store
+                    .dispatch("category/getTreeList", {filter: {categoryType: 9}})
+                    .then(data => {
+                        if (data) {
+                            this.categoryList = this.getTreeData(data.data);
+                        }
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        console.log(error);
+                    });
+            },
+            getTreeData(data) {
+                // 循环遍历json数据
+                for (let i = 0, len = data.length; i < len; i++) {
+                    if (data[i].children.length < 1) {
+                        // children若为空数组，则将children设为undefined
+                        data[i].children = undefined;
+                    } else {
+                        // children若不为空数组，则继续 递归调用 本方法
+                        this.getTreeData(data[i].children);
+                    }
+                }
+                return data;
+            },
+            handleCategory(category) {
+                if (category) {
+                    let code = category[category.length - 1];
+                    this.formData.categoryCode = code;
+                    let param = {};
+                    param.categoryCode = code;
+                    this.loadpropertyList(param);
+                }
+            },
+            //跳转回列表页面
+            goBack() {
+                if (this.$router.history.length <= 1) {
+                    this.$router.push({path: '/home'});
+                    return false;
+                } else {
+                    this.$router.go(-1);
+                }
             }
-            this.loading = false;
-          })
-          .catch(error => {
-            this.loading = false;
-            console.log(error);
-          });
-      },
-      getTreeData(data) {
-        // 循环遍历json数据
-        for (let i = 0, len = data.length; i < len; i++) {
-          if (data[i].children.length < 1) {
-            // children若为空数组，则将children设为undefined
-            data[i].children = undefined;
-          } else {
-            // children若不为空数组，则继续 递归调用 本方法
-            this.getTreeData(data[i].children);
-          }
+        },
+        created() {
+            if (this.$route.query.productId) {
+                this.handleGetOne(this.$route.query.productId);
+            }
+            this.loadTreeData();
+            this.loadBrand();
         }
-        return data;
-      },
-      handleCategory(category) {
-        if (category) {
-          let code = category[category.length - 1];
-          this.formData.categoryCode = code;
-          let param = {};
-          param.categoryCode = code;
-          this.loadpropertyList(param);
-        }
-      },
-      //跳转回列表页面
-      goBack() {
-        if (this.$router.history.length <= 1) {
-          this.$router.push({path: '/home'});
-          return false;
-        } else {
-          this.$router.go(-1);
-        }
-      }
-    },
-    created() {
-      if (this.$route.query.productId) {
-        this.handleGetOne(this.$route.query.productId);
-      }
-      this.loadTreeData();
-      this.loadBrand();
     }
-  }
 </script>
