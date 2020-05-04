801f4459a4069e9c314d13d637b847b1a3270c5d
杨绍宾 <1182705849@qq.com>
2020-05-04 21:02:44 +0800

    airport

--- src/views/order/GoTicket.vue
+++ src/views/order/GoTicket.vue
@@ -421,7 +421,7 @@ export default {
                     amountTotal += Number(item.amount);
                   });
                   this.systemProfitAndLossValue = 0;
-                  this.systemProfitAndLossValue = Number(this.payData.noPayAmount) - Number(amountTotal);
+                  this.systemProfitAndLossValue = Number(amountTotal) - Number(this.payData.noPayAmount);
                   this.$message({
                     type: "success",
                     message: "预定成功！"
