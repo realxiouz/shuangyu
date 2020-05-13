47984848fdba5895e065b64340714c5dd7cd60f3
付海波 <541538356@qq.com>
2020-05-13 11:19:02 +0800

    dd

---
+++ src/views/productFeature/Edit.vue
@@ -0,0 +1,480 @@
+<template>
+  <div>
+    <div id="goBack" @click="goBack">
+      <el-page-header></el-page-header>
+    </div>
+    <el-card class="contentBox">
+      <div slot="header">
+        <span>商品属性管理</span>
+      </div>
+      <el-form ref="form" :rules="rules" :model="formData" label-width="130px" size="mini">
+        <el-row :gutter="10">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="商品类目" prop="categoryName">
+              <el-input v-model="formData.categoryName" disabled></el-input>
+            </el-form-item>
+            <!--            <el-form-item label="商品类目">-->
+            <!--              <el-cascader-->
+            <!--                v-model="formData.categoryCode"-->
+            <!--                style="width: 100%;"-->
+            <!--                :options="categoryList"-->
+            <!--                :props="{ label: 'categoryName', value: 'categoryCode' }"-->
+            <!--                filterable-->
+            <!--                @change="handleCategory"-->
+            <!--              >-->
+            <!--              </el-cascader>-->
+            <!--            </el-form-item>-->
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="属性编码" prop="propertyCode">
+              <el-input v-model="formData.propertyCode"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="属性名称" prop="propertyLabel">
+              <el-input v-model="formData.propertyLabel"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="属性类型:" prop="valueType">
+              <!-- 数据类型（0文本，1开关，2数字，3日期，4日期时间，5时间，6评分，7单选，8多选，9选择器）-->
+              <el-select clearable v-model="formData.valueType" placeholder="全部" style="width: 100%"
+                         @change="valueTypeChange">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==8">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="是否销售属性" prop="sku">
+              <el-switch v-model="formData.sku" :active-value=true :inactive-value=false
+              ></el-switch>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==2">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="精度" prop="precision">
+              <el-input v-model="formData.precision" @change="changeNum"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==0">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:" prop="precision">
+              <el-input v-model="test.input" placeholder="请输入内容"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==1">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-switch
+                v-model="test.switchValue"
+              >
+              </el-switch>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==2">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-input-number v-model="test.num" :precision="formData.precision"></el-input-number>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==3">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-date-picker
+                v-model="test.time1"
+                type="date"
+                placeholder="选择日期">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==4">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-date-picker
+                v-model="test.time2"
+                type="datetime"
+                placeholder="选择日期时间">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==5">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-time-select
+                v-model="test.time3"
+                :picker-options="{
+    start: '08:30',
+    step: '00:15',
+    end: '18:30'
+  }"
+                placeholder="选择时间">
+              </el-time-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==6">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-rate v-model="test.rateValue"></el-rate>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==7">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-radio v-model="test.radio" label="1">单选项1</el-radio>
+              <el-radio v-model="test.radio" label="2">单选项2</el-radio>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==8">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-checkbox v-model="test.checked1">多选框1</el-checkbox>
+              <el-checkbox v-model="test.checked2">多选框2</el-checkbox>
+              <el-checkbox v-model="test.checked3">多选框3</el-checkbox>
+              <el-checkbox v-model="test.checked4">多选框4</el-checkbox>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10" v-if="valueType ==9">
+          <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
+            <el-form-item label="示例:">
+              <el-select
+                style="width: 100%;"
+                clearable
+                collapse-tags
+                v-model="test.selectValue"
+                placeholder="请选择"
+              >
+                <el-option label="选项1" value="0"></el-option>
+                <el-option label="选项2" value="1"></el-option>
+                <el-option label="选项3" value="1"></el-option>
+                <el-option label="选项4" value="1"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <div v-show="showAddValues">
+          <el-button icon="el-icon-plus" type="primary" size="mini" @click="handleAdd">添加属性值</el-button>
+          <el-table
+            :data="values"
+            style="width: 100%;margin-bottom: 15px;"
+            size="mini"
+            border
+          >
+            <el-table-column prop="code" label="编码" align="center"></el-table-column>
+            <el-table-column prop="value" label="值" align="center"></el-table-column>
+            <el-table-column label="操作" align="center" width="350">
+              <template slot-scope="scope">
+                <el-button type="danger" size="mini"
+                           @click="valueRemove(scope.row.code,scope.$index,values)">删除
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+        <el-dialog title="属性值" :visible.sync="dialogVisible" width="20%">
+          <el-form ref="form" :model="formValue" label-width="90px">
+            <el-form-item label="编码">
+              <el-input v-model="formValue.code"></el-input>
+            </el-form-item>
+            <el-form-item label="值">
+              <el-input v-model="formValue.value"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="valueCancel">取 消</el-button>
+            <el-button type="primary" @click="valueSave">确 定</el-button>
+          </div>
+        </el-dialog>
+      </el-form>
+    </el-card>
+    <div slot="footer" style="text-align:center;">
+      <el-button size="mini" type="primary" @click="handleCancel">取消</el-button>
+      <el-button size="mini" type="primary" @click="handleSave">保存</el-button>
+    </div>
+  </div>
+</template>
+<script>
+    function defaultData() {
+        return {
+            categoryCode: "",
+            categoryName: "",
+            categoryPath: "",
+            propertyCode: "",
+            propertyLabel: "",
+            precision: 0,
+            sku: false,
+            valueType: ''
+        }
+    };
+    export default {
+        name: 'edit',
+        data() {
+            const categoryCode = (rule, value, callback) => {
+                let reg = /^[0-9a-zA-Z]*$/g;
+                if (reg.test(value)) {
+                    callback();
+                } else {
+                    callback(new Error("只能输入字母或数字！"));
+                }
+            };
+            return {
+                formData: defaultData(),
+                dialogVisible: false,
+                showAddValues: false,
+                formValue: {},
+                propertyId: '',
+                valueType: '',
+                test: {},
+                values: [],
+                categoryList: [],
+                rules: {
+                    categoryName: [
+                        {required: true, message: "请输入商品类目", trigger: "blur"},
+                    ],
+                    propertyCode: [
+                        {required: true, message: "请输入属性编码", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到 20 个字符"
+                        },
+                        {validator: categoryCode, trigger: 'blur'}
+                    ],
+                    propertyLabel: [
+                        {required: true, message: "请输入属性标题", trigger: "blur"},
+                        {
+                            min: 1,
+                            max: 20,
+                            message: "长度在 1到 20 个字符"
+                        }
+                    ]
+                    , valueType: [
+                        {required: true, message: "请选择属性类型", trigger: "blur"},
+                    ]
+                },
+                options: [{
+                    value: 0,
+                    label: '文本'
+                },
+                    {
+                        value: 1,
+                        label: '开关'
+                    },
+                    {
+                        value: 2,
+                        label: '数字'
+                    },
+                    {
+                        value: 3,
+                        label: '日期'
+                    },
+                    {
+                        value: 4,
+                        label: '日期时间'
+                    },
+                    {
+                        value: 5,
+                        label: '时间'
+                    },
+                    {
+                        value: 6,
+                        label: '评分'
+                    },
+                    {
+                        value: 7,
+                        label: '单选'
+                    },
+                    {
+                        value: 8,
+                        label: '多选'
+                    },
+                    {
+                        value: 9,
+                        label: '选择器'
+                    }]
+            }
+        },
+        methods: {
+            changeNum(value) {
+                if (value > 0) {
+                    this.formData.precision = value;
+                }
+            },
+            valueTypeChange(value) {
+                this.valueType = value;
+                if (value == 7 || value == 8 || value == 9) {
+                    this.showAddValues = true;
+                } else {
+                    this.showAddValues = false;
+                }
+            },
+            //跳转回列表页面
+            goBack() {
+                if (this.$router.history.length <= 1) {
+                    this.$router.push({path: '/home'});
+                    return false;
+                } else {
+                    this.$router.go(-1);
+                }
+            },
+            handleCancel() {
+                this.goBack();
+            },
+            handleSave() {
+                this.$refs['form'].validate((valid) => {
+                    if (valid) {
+                        if (this.values.length > 0) {
+                            let map = {};
+                            for (let i = 0, len = this.values.length; i < len; i++) {
+                                map[this.values[i].code] = this.values[i].value;
+                            }
+                            this.formData.values = map;
+                        }
+                        this.$store
+                            .dispatch("productProperty/save", {
+                                property: this.formData,
+                                valueList: this.values
+                            })
+                            .then(() => {
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            });
+                        this.$message({
+                            type: "success",
+                            message: "保存成功！"
+                        });
+                        this.goBack();
+                    }
+                });
+            },
+            handleGetOne(id) {
+                if (id) {
+                    this.$store
+                        .dispatch("productProperty/getOne", {propertyId: id})
+                        .then(data => {
+                            if (data) {
+                                this.formData = data;
+                                this.valueTypeChange(this.formData.valueType);
+                                if (data.values != null) {
+                                    let array = [];
+                                    let map = data.values;
+                                    for (let key in map) {
+                                        let data = {};
+                                        data.code = key;
+                                        data.value = map[key];
+                                        array.push(data)
+                                    }
+                                    this.values = array;
+                                }
+                            }
+                        }).catch(error => {
+                        console.log(error);
+                    });
+                } else {
+                    this.formData = defaultData();
+                }
+            },
+            valueRemove(id, index, rows) {
+                this.$confirm("此操作将删除该条记录, 是否继续?", "提示", {
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    type: "warning"
+                })
+                    .then(() => {
+                        rows.splice(index, 1);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+            },
+            handleAdd() {
+                this.dialogVisible = true;
+            },
+            valueCancel() {
+                this.dialogVisible = false;
+            },
+            valueSave() {
+                if (!this.formValue.code || !this.formValue.value) {
+                    this.$message("请填写完整属性信息！")
+                    return false;
+                }
+                this.values.push(this.formValue);
+                this.dialogVisible = false;
+                this.formValue = {};
+            },
+            loadTreeData() {
+                this.$store
+                    .dispatch("category/getTreeList", {filter: {categoryType: 9}})
+                    .then(data => {
+                        if (data) {
+                            this.categoryList = this.getTreeData(data.data);
+                        }
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        console.log(error);
+                    });
+            },
+            getTreeData(data) {
+                // 循环遍历json数据
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].children.length < 1) {
+                        // children若为空数组，则将children设为undefined
+                        data[i].children = undefined;
+                    } else {
+                        // children若不为空数组，则继续 递归调用 本方法
+                        this.getTreeData(data[i].children);
+                    }
+                }
+                return data;
+            },
+            handleCategory(category) {
+                if (category) {
+                    let code = category[category.length - 1];
+                    this.formData.categoryCode = code;
+                }
+            },
+        },
+        created() {
+            if (this.$route.query.propertyId) {
+                this.propertyId = this.$route.query.propertyId;
+                this.handleGetOne(this.$route.query.propertyId);
+            }
+            if (this.$route.query.categoryCode) {
+                this.formData.categoryCode = this.$route.query.categoryCode;
+            }
+            if (this.$route.query.categoryName) {
+                this.formData.categoryName = this.$route.query.categoryName;
+            }
+            if (this.$route.query.categoryPath) {
+                this.formData.categoryPath = this.$route.query.categoryPath;
+            }
+            this.loadTreeData();
+        }
+    }
+</script>
---
+++ src/views/productFeature/List.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="contentBox">
+    <el-row :gutter="20">
+      <el-col :xs="11" :sm="10" :md="9" :lg="8" :xl="8">
+        <el-row style="margin-bottom:20px;">
+          <span style="font-weight:700;color:#303133;">商品类目</span>
+        </el-row>
+        <el-tree
+          v-loading="loading"
+          node-key="categoryId"
+          auto-expand-parent
+          :data="treeData"
+          :default-expanded-keys="curLine"
+          :props="treeProps"
+          :highlight-current="true"
+          @node-click="handleNodeClick"
+        >
+          <span class="tree-node" slot-scope="{ node}">
+            <span>{{ node.data.categoryName }}</span>
+          </span>
+        </el-tree>
+      </el-col>
+      <el-col :xs="13" :sm="14" :md="15" :lg="16" :xl="16">
+        <!--        <div class="searchBox">-->
+        <!--          <search @onSearch="handleSearch"></search>-->
+        <!--        </div>-->
+        <el-row type="flex" justify="space-between" style="margin-bottom:20px;" align="bottom">
+          <el-button icon="el-icon-plus" type="primary" size="mini" @click="handleAdd" :disabled="dialogVisible">添加属性
+          </el-button>
+        </el-row>
+        <el-table
+          v-loading="loading"
+          :data="tableData"
+          style="width: 100%;margin-bottom: 15px;"
+          size="mini"
+        >
+          <el-table-column prop="categoryName" label="商品类目" align="center"></el-table-column>
+          <el-table-column prop="propertyLabel" label="属性名称" align="center"></el-table-column>
+          <el-table-column prop="propertyCode" label="属性编码" align="center"></el-table-column>
+          <el-table-column prop="isSku" label="是否销售属性" align="center">
+            <template slot-scope="scope">
+              <el-switch v-model="scope.row.sku" disabled></el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column fixed="right" label="操作" align="center" width="350">
+            <template slot-scope="scope">
+              <el-button @click="handleUpdate(scope.row.propertyId)" type="primary" size="mini">编辑</el-button>
+              <el-button
+                @click.native.prevent="handleRemove(scope.row.propertyId,scope.$index,tableData)"
+                type="danger"
+                size="mini"
+              >删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @prev-click="prevClick"
+          @next-click="nextClick"
+          background
+          layout="total,sizes,prev,next"
+          prev-text="上一页"
+          next-text="下一页"
+          :page-size="pageSize"
+          :total="total"
+        ></el-pagination>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+    import search from "./Search";
+
+    export default {
+        data() {
+            return {
+                loading: true,
+                curNode: null,
+                tableData: [],
+                treeData: [],
+                curLine: [],
+                treeProps: {
+                    children: "children",
+                    hasChildren: "xxx"
+                },
+                lastId: "0",
+                pageFlag: "next",
+                pageSize: 10,
+                dialogVisible: true,
+                propertyId: "",
+                total: 0
+            };
+        },
+        methods: {
+            /*加载类别树*/
+            loadTreeData() {
+                this.$store
+                    .dispatch("category/getTreeList", {filter: {categoryType: 9}})
+                    .then(data => {
+                        if (data) {
+                            this.treeData = data.data;
+                        }
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        console.log(error);
+                    });
+            },
+            /*点击部门树时调用*/
+            handleNodeClick(data) {
+                this.dialogVisible = false;
+                this.curNode = data;
+                let searchForm = {};
+                searchForm.categoryCode = data.categoryCode;
+                this.lastId = '0';
+                this.loadData(searchForm);
+            },
+            prevClick() {
+                this.pageFlag = "prev";
+                this.lastId = this.tableData[0].propertyId;
+                this.loadData();
+            },
+            nextClick() {
+                this.pageFlag = "next";
+                this.lastId = this.tableData[this.tableData.length - 1].propertyId;
+                this.loadData();
+            },
+            loadTotal(searchForm) {
+                this.$store
+                    .dispatch("productProperty/getTotal", {
+                        filter: searchForm
+                    })
+                    .then(data => {
+                        this.total = data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            loadData(searchForm) {
+                this.$store
+                    .dispatch("productProperty/getPageList", {
+                        pageFlag: this.pageFlag,
+                        pageSize: this.pageSize,
+                        lastId: this.lastId,
+                        filter: searchForm
+                    })
+                    .then(data => {
+                        if (data) {
+                            this.tableData = data;
+                            this.loadTotal(searchForm);
+                        }
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        console.log(error);
+                    });
+            },
+            handleAdd() {
+                let path = "";
+                path = "/property/config";
+                this.$router.push({
+                    path: path,
+                    query: {
+                        categoryCode: this.curNode.categoryCode,
+                        categoryName: this.curNode.categoryName,
+                        categoryPath: this.curNode.path
+                    }
+                });
+            },
+            handleUpdate(id) {
+                let path = "";
+                path = "/property/config";
+                this.$router.push({
+                    path: path,
+                    query: {
+                        propertyId: id
+                    }
+                });
+            },
+            handleRemove(id, index, rows) {
+                this.$confirm("此操作将状态改为删除状态, 是否继续?", "提示", {
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    type: "warning"
+                })
+                    .then(() => {
+                        this.$store.dispatch("productProperty/removeOne", {propertyId: id}).then(() => {
+                            if (1 === this.tableData.length) {
+                                this.prevClick();
+                            } else {
+                                this.loadData();
+                            }
+                            rows.splice(index, 1);
+                        });
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+            },
+            handleSizeChange(pageSize) {
+                this.pageSize = pageSize;
+                this.loadData();
+            },
+            handleSearch(params) {
+                const newParams = {};
+                if (params) {
+                    for (let key in params) {
+                        if (params[key]) {
+                            newParams[key] = params[key];
+                        }
+                    }
+                }
+                if (Object.keys(newParams).length == 0) {
+                    this.lastId = 0;
+                }
+                this.loadData(newParams);
+                this.$message({
+                    type: "success",
+                    message: "查询成功！"
+                });
+            },
+        },
+        created() {
+            this.loadTreeData();
+            this.loadData();
+        },
+        components: {
+            search
+        }
+    };
+</script>
