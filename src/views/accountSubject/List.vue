50a39abfde1ce38875fb5a48dc5f810606a9cc22
李思韩 <1915844250@qq.com>
2020-05-15 17:51:49 +0800

    样式修改

--- src/views/accountSubject/List.vue
+++ src/views/accountSubject/List.vue
@@ -2,39 +2,39 @@
   <div>
     <el-tabs type="border-card" @tab-click="handleClick">
       <el-tab-pane label="资产">
-        <subject-content v-if="0 === category" :category="category"/>
+        <subject-content v-if="0 === category" :category="category" />
       </el-tab-pane>
       <el-tab-pane label="负债">
-        <subject-content v-if="1 === category" :category="category"/>
+        <subject-content v-if="1 === category" :category="category" />
       </el-tab-pane>
       <el-tab-pane label="权益">
-        <subject-content v-if="2 === category" :category="category"/>
+        <subject-content v-if="2 === category" :category="category" />
       </el-tab-pane>
       <el-tab-pane label="成本">
-        <subject-content v-if="3 === category" :category="category"/>
+        <subject-content v-if="3 === category" :category="category" />
       </el-tab-pane>
       <el-tab-pane label="损益">
-        <subject-content v-if="4 === category" :category="category"/>
+        <subject-content v-if="4 === category" :category="category" />
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
-    import subjectContent from "./Content.vue";
-    export default {
-        data() {
-            return {
-                category: 0
-            };
-        },
-        methods: {
-            handleClick(item){
-                this.category = parseInt(item.index);
-            }
-        },
-        components: {
-            subjectContent
-        }
+import subjectContent from "./Content.vue";
+export default {
+  data() {
+    return {
+      category: 0
     };
+  },
+  methods: {
+    handleClick(item) {
+      this.category = parseInt(item.index);
+    }
+  },
+  components: {
+    subjectContent
+  }
+};
 </script>
