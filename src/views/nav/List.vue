60eaa0b7aa416bb74e370b85674a0d4eef758c0b
蒋体祥 <642172707@qq.com>
2020-05-22 14:49:00 +0800

    导航菜单排序

--- src/views/nav/List.vue
+++ src/views/nav/List.vue
@@ -13,8 +13,9 @@
           @node-click="handleNodeClick"
         >
           <span class="tree-node" slot-scope="{ node, data }">
-             <span>{{ node.data.title }} ({{ node.data.sort }})</span>
+             <span>{{ node.data.title }}</span>
             <span>
+               <span>{{ node.data.sort }}&nbsp;&nbsp;&nbsp;</span>
               <el-button type="text" size="mini" @click="nodeAdd(node, data)">添加</el-button>
               <el-button type="text" size="mini" @click="handleEdit(node, data)">修改</el-button>
               <el-button type="text" size="mini" @click="removeNode(node, data)">移除</el-button>
