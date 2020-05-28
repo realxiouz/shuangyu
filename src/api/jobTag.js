f41be41ba40604af5e4fd1e978c9a24a22d6131a
杨绍宾 <1182705849@qq.com>
2020-05-28 17:08:29 +0800

    根据供应商id判断蜗牛导单

---
+++ src/api/jobTag.js
@@ -0,0 +1,67 @@
+import request from '@/utils/request';
+
+export function addOne(data) {
+  return request({
+    url: `/job/tag/add/one`,
+    method: 'post',
+    data
+  });
+}
+
+export function removeOne(jobTagId) {
+  return request({
+    url: `/job/tag/remove/one/${jobTagId}`,
+    method: 'delete'
+  });
+}
+
+export function updateOne(jobTagId, data) {
+  return request({
+    url: `/job/tag/update/one/${jobTagId}`,
+    method: 'put',
+    data
+  });
+}
+
+export function updateMany(filter, data) {
+  return request({
+    url: `/job/tag/update/many`,
+    method: 'put',
+    params: filter,
+    data
+  });
+}
+
+export function getOne(jobTagId) {
+  return request({
+    url: `/job/tag/one/${jobTagId}`,
+    method: 'get'
+  });
+}
+
+export function getList(filter) {
+  return request({
+    url: '/job/tag/list',
+    method: 'get',
+    params: filter
+  });
+}
+
+export function getTotal(filter) {
+  return request({
+    url: `/job/tag/total`,
+    method: 'get',
+    params: filter
+  });
+}
+
+export function getPageList(pageFlag, pageSize, filter) {
+  return request({
+    url: `/job/tag/page/list/${pageFlag}/${pageSize}`,
+    method: 'get',
+    params: filter
+  });
+}
+
+
+
