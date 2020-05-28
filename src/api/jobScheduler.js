3e49eaf8cb2a529d2e3ab2c5115a3b30f0afdb60
杨绍宾 <1182705849@qq.com>
2020-05-28 18:25:23 +0800

    根据供应商id判断蜗牛导单

---
+++ src/api/jobScheduler.js
@@ -0,0 +1,67 @@
+import request from '@/utils/request';
+
+export function addOne(data) {
+  return request({
+    url: `/job/scheduler/add/one`,
+    method: 'post',
+    data
+  });
+}
+
+export function removeOne(jobSchedulerId) {
+  return request({
+    url: `/job/scheduler/remove/one/${jobSchedulerId}`,
+    method: 'delete'
+  });
+}
+
+export function updateOne(jobSchedulerId, data) {
+  return request({
+    url: `/job/scheduler/update/one/${jobSchedulerId}`,
+    method: 'put',
+    data
+  });
+}
+
+export function updateMany(filter, data) {
+  return request({
+    url: `/job/scheduler/update/many`,
+    method: 'put',
+    params: filter,
+    data
+  });
+}
+
+export function getOne(jobSchedulerId) {
+  return request({
+    url: `/job/scheduler/one/${jobSchedulerId}`,
+    method: 'get'
+  });
+}
+
+export function getList(filter) {
+  return request({
+    url: '/job/scheduler/list',
+    method: 'get',
+    params: filter
+  });
+}
+
+export function getTotal(filter) {
+  return request({
+    url: `/job/scheduler/total`,
+    method: 'get',
+    params: filter
+  });
+}
+
+export function getPageList(pageFlag, pageSize, filter) {
+  return request({
+    url: `/job/scheduler/page/list/${pageFlag}/${pageSize}`,
+    method: 'get',
+    params: filter
+  });
+}
+
+
+
---
+++ src/store/modules/jobConfig.js
@@ -0,0 +1,103 @@
+import {
+  addOne,
+  getList,
+  getOne,
+  getPageList,
+  getTotal,
+  removeOne,
+  updateOne
+} from "@/api/jobConfig";
+
+const actions = {
+  addOne({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {jobConfig} = params
+      addOne(jobConfig)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  removeOne({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {jobConfigId} = params
+      removeOne(jobConfigId)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  updateOne({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {jobConfigId, data} = params;
+      updateOne(jobConfigId, data)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getOne({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {jobConfigId} = params;
+      getOne(jobConfigId)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getList({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {filter} = params;
+      getList(filter)
+        .then(response => {
+          const {data} = response;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getTotal({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {filter} = params;
+      getTotal(filter)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getPageList({commit}, params) {
+    return new Promise((resolve, reject) => {
+      const {pageFlag, pageSize, filter} = params;
+      getPageList(pageFlag, pageSize, filter)
+        .then(response => {
+          resolve(response);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+};
+
+export default {
+  namespaced: true,
+  actions
+};
