ecd2a6be058cec0cfd8f76f8ff1a735709aec9e9
孙林凡 <linfansun@163.com>
2020-06-03 13:36:36 +0800

    product

--- src/views/product/List.vue
+++ src/views/product/List.vue
@@ -48,8 +48,8 @@
       </el-table>
       <el-pagination
         @size-change="handleSizeChange"
-        @prev-click="prevClick"
-        @next-click="nextClick"
+        @prev-click="handlePrevClick"
+        @next-click="handleNextClick"
         background
         layout="total,sizes,prev,next"
         prev-text="上一页"
@@ -68,24 +68,26 @@
         name: "list",
         data() {
             return {
-                lastId: "0",
-                pageFlag: "next",
+                pageFlag: 1,
                 pageSize: 10,
+                lastId: null,
+                total: 0,
                 dialogVisible: false,
                 loading: true,
                 tableData: [],
-                productId: "",
-                total: 0
+                productId: ""
             };
         },
         methods: {
-            prevClick() {
-                this.pageFlag = "prev";
+            /*翻前页*/
+            handlePrevClick() {
+                this.pageFlag = -1;
                 this.lastId = this.tableData[0].productId;
                 this.loadData();
             },
-            nextClick() {
-                this.pageFlag = "next";
+            /*翻后页*/
+            handleNextClick() {
+                this.pageFlag = 1;
                 this.lastId = this.tableData[this.tableData.length - 1].productId;
                 this.loadData();
             },
@@ -101,12 +103,14 @@
                         console.log(error);
                     });
             },
-            loadData(searchForm) {
+            loadData(searchForm = {}) {
+                if (this.lastId) {
+                    searchForm.lastId = this.lastId;
+                }
                 this.$store
                     .dispatch("product/getPageList", {
                         pageFlag: this.pageFlag,
                         pageSize: this.pageSize,
-                        lastId: this.lastId,
                         filter: searchForm
                     })
                     .then(data => {
