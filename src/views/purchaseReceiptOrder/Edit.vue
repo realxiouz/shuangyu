3676572f8d5d6010ba92b4fb965001cab0efce50
孙林凡 <linfansun@163.com>
2020-06-09 16:41:34 +0800

    采购单

--- src/views/purchaseReceiptOrder/Edit.vue
+++ src/views/purchaseReceiptOrder/Edit.vue
@@ -314,12 +314,6 @@
                     fundAccountCode: '',
                     //结算账户名称
                     fundAccountName: '',
-                    //制单人
-                    recordId: '',
-                    //制单时间
-                    recordDate: new Date(),
-                    //制单人姓名
-                    recordName: '',
                     //备注
                     remark: ''
                 };
