bed57f35be6b302a022543ff0bdf52765b0a6c69
sunlinfan <linfansun@163.com>
2020-06-17 18:52:48 +0800

    税率输入验证，手动出票

--- src/api/trade.js
+++ src/api/trade.js
@@ -1,9 +1,28 @@
 import request from '@/utils/request';
 
+export function save(data) {
+  return request({
+    url: "/finance/trade/save/one",
+    method: "post",
+    data
+  });
+}
+export function getOne(id) {
+  return request({
+    url: `/finance/trade/get/one/${id}`,
+    method: 'get'
+  });
+}
+export function removeOne(id) {
+  return request({
+    url: `/finance/trade/remove/one/${id}`,
+    method: "delete"
+  });
+}
 
 export function getList(filter) {
   return request({
-    url: `/trade/list`,
+    url: `/finance/trade/get/list`,
     method: 'get',
     params: filter
   });
@@ -11,7 +30,7 @@ export function getList(filter) {
 
 export function getTotal(filter) {
   return request({
-    url: '/trade/total',
+    url: '/finance/trade/get/total',
     method: 'get',
     params: filter
   });
@@ -19,7 +38,7 @@ export function getTotal(filter) {
 
 export function getPageList(pageFlag, pageSize, lastId, filter) {
   return request({
-    url: `/trade/page/list/${pageFlag}/${pageSize}/${lastId}`,
+    url: `/finance/trade/get/page/list/${pageFlag}/${pageSize}/${lastId}`,
     method: 'get',
     params: filter
   });
