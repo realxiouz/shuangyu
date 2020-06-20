9c19abff3eae3845b58ba6d399fc2a021d42d37f
孙林凡 <linfansun@163.com>
2020-06-20 17:23:10 +0800

    /firm

--- src/api/firmOpenAuth.js
+++ src/api/firmOpenAuth.js
@@ -3,14 +3,14 @@ import request from '@/utils/request';
 
 export function removeOne(id) {
   return request({
-    url: `/open/auth/remove/one/${id}`,
+    url: `/firm/open/auth/remove/one/${id}`,
     method: 'delete'
   });
 }
 
 export function updateOne(id, data) {
   return request({
-    url: `/open/auth/update/one${id}`,
+    url: `/firm/open/auth/update/one${id}`,
     method: 'put',
     data
   });
@@ -18,7 +18,7 @@ export function updateOne(id, data) {
 
 export function getPageList(pageFlag, pageSize, filter) {
   return request({
-    url: `/open/auth/get/page/list/${pageFlag}/${pageSize}`,
+    url: `/firm/open/auth/get/page/list/${pageFlag}/${pageSize}`,
     method: 'get',
     params: filter
   });
@@ -26,7 +26,7 @@ export function getPageList(pageFlag, pageSize, filter) {
 
 export function getCustomerList(params) {
   return request({
-    url: `/customer/open/auth/get/list`,
+    url: `/firm/customer/open/auth/get/list`,
     method: 'get',
     params: params
   });
@@ -34,7 +34,7 @@ export function getCustomerList(params) {
 
 export function getSupplierList(params) {
   return request({
-    url: `/supplier/open/auth/get/list`,
+    url: `/firm/supplier/open/auth/get/list`,
     method: 'get',
     params: params
   });
@@ -42,7 +42,7 @@ export function getSupplierList(params) {
 
 export function getTotal(params) {
   return request({
-    url: `/open/auth/get/total`,
+    url: `/firm/open/auth/get/total`,
     method: 'get',
     params: params
   });
@@ -50,14 +50,14 @@ export function getTotal(params) {
 
 export function getOne(id) {
   return request({
-    url: `/open/auth/get/one/${id}`,
+    url: `/firm/open/auth/get/one/${id}`,
     method: 'get'
   });
 }
 
 export function addOne(data) {
   return request({
-    url: "/open/auth/add/one",
+    url: "/firm/open/auth/add/one",
     method: "post",
     data
   });
