942df4a449c0ac2a15be04a3b8cc3bbe14929570
付海波 <541538356@qq.com>
2020-07-08 18:28:46 +0800

    调制布局

--- src/views/manage/List.vue
+++ src/views/manage/List.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="page">
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>模拟订单通知</span>
-        <el-button style="float: right; padding: 3px 0" type="text" @click="orderNotify">通知</el-button>
-      </div>
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="调用地址">
-          <el-input v-model="formData.notifyUrl"></el-input>
-        </el-form-item>
-        <el-form-item label="订单编号">
-          <el-input v-model="formData.orderNo"></el-input>
-        </el-form-item>
-        <el-form-item label="状态编码">
-          <el-select v-model="formData.changeCode" placeholder="请选择状态编码">
-            <el-option label="0401" value="shanghai"></el-option>
-            <el-option label="0402" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-    </el-card>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>模拟订单通知</span>
+            <el-button style="float: right; padding: 3px 0" type="text" @click="orderNotify">通知</el-button>
+          </div>
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="调用地址">
+              <el-input v-model="formData.notifyUrl"></el-input>
+            </el-form-item>
+            <el-form-item label="订单编号">
+              <el-input v-model="formData.orderNo"></el-input>
+            </el-form-item>
+            <el-form-item label="状态编码">
+              <el-select v-model="formData.changeCode" placeholder="请选择状态编码" style="width: 100%">
+                <el-option label="0401" value="shanghai"></el-option>
+                <el-option label="0402" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-col>
+      <el-col :span="8"></el-col>
+      <el-col :span="8"></el-col>
+    </el-row>
+
   </div>
 </template>
 
