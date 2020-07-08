cf86cca68882a4fc57bf2e024a651ecacdecf840
付海波 <541538356@qq.com>
2020-07-08 16:36:18 +0800

    Merge branch 'dev' into fixTotal
    # Conflicts:
    #	src/views/airlineChange/List.vue
    #	src/views/product/Add.vue

---
+++ src/views/manage/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>模拟订单通知</span>
+        <el-button style="float: right; padding: 3px 0" type="text">通知</el-button>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="订单编号">
+          <el-input v-model="formData.orderNo"></el-input>
+        </el-form-item>
+        <el-form-item label="状态编码">
+          <el-select v-model="formData.StateCode" placeholder="请选择状态编码">
+            <el-option label="0401" value="shanghai"></el-option>
+            <el-option label="0402" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "List",
+    data() {
+      return {
+        formData: {
+          orderNo: "",
+          StateCode: ""
+        }
+      };
+    },
+    methods: {}
+  };
+</script>
