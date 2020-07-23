6920b9345a27356b8bb6700e37d1ec011337fd4b
秦博 <xiouz@163.com>
2020-07-23 14:31:58 +0800

    .

--- src/views/productPurchaseOrder/List.vue
+++ src/views/productPurchaseOrder/List.vue
@@ -24,14 +24,13 @@
                 <template slot-scope="props">
                   <el-form label-position="right" :inline="true" label-width="120px">
                     <div class="detail">
-                      <div :v-if="props.row.propertyItems.length >0"
-                           v-for="(item, index) in props.row.propertyItems"
-                           :key="index">
-                        <el-form-item :label="item.name +':'" v-if="item.code != 'flightDate'">
-                          <span>{{ item.value }}</span>
-                        </el-form-item>
-                        <el-form-item :label="item.name +':'" v-if="item.code == 'flightDate'">
-                          <span>    {{initDate(item.value, 'YYYY-MM-DD')}}</span>
+                      <div
+                        :v-if="props.row.propertyItems.length > 0"
+                        v-for="(i, index) in props.row.propertyItems"
+                        :key="index"
+                      >
+                        <el-form-item :label="`${i.name}:`" v-if="!i.hidden">
+                          <span>{{ getValByType(i) }}</span>
                         </el-form-item>
                       </div>
                     </div>
@@ -47,6 +46,26 @@
               <el-table-column prop="unit" label="计量单位" align="center"></el-table-column>
               <el-table-column prop="amount" label="金额" align="center">
               </el-table-column>
+            </el-table>
+
+            <el-table
+              :data="scope.row.passengers"
+              border
+              style="margin-top:10px;"
+            >
+              <el-table-column label="乘客类型" width="70">
+                <template v-slot="{ row }">
+                  <el-tag type="primary">{{ ageMap[row.ageType] }}</el-tag>
+                </template>
+              </el-table-column>
+              <el-table-column label="姓名" prop="fullName" />
+              <el-table-column label="电话" prop="phone" width="120" />
+              <el-table-column label="证件类型" width="90">
+                <template v-slot="{ row }">
+                  <el-tag type="primary">{{ cardMap[row.idCardType] }}</el-tag>
+                </template>
+              </el-table-column>
+              <el-table-column label="证件号" prop="idCardNo" />
             </el-table>
           </template>
         </el-table-column>
@@ -71,10 +90,10 @@
             <el-button v-show="scope.row.orderStatus != 0" @click="skipDetail(scope.row.orderNo)" type="primary"
                                        size="mini">查看
           </el-button>
-            <el-button v-show="scope.row.orderStatus == 0" @click="onEdit(scope.row)" type="primary" size="mini">
+            <el-button v-show="scope.row.orderStatus == 0" @click="onEdit(scope.row.orderNo)" type="primary" size="mini">
               编辑
             </el-button>
-            <el-button v-show="scope.row.orderStatus == 0" @click="onDel(scope.row)" type="danger" size="mini">
+            <el-button v-show="scope.row.orderStatus == 0" @click="onDel(scope.row.orderNo)" type="danger" size="mini">
               删除
             </el-button>
           </template>
@@ -106,14 +125,15 @@
     } from "@/utils/productStatus.js";
 
     import { MIXIN_LIST } from "@/utils/mixin";
+    import { CARD_TYPES_MAP, AGE_TYPES_MAP } from "@/utils/const";
 
 
     export default {
         mixins: [MIXIN_LIST],
         data() {
             return {
-                // cardMap: CARD_TYPES_MAP,
-                // ageMap: AGE_TYPES_MAP,
+                cardMap: CARD_TYPES_MAP,
+                ageMap: AGE_TYPES_MAP,
 
                 keyId: "",
                 keyName: "orderNo",
@@ -134,8 +154,8 @@
             handleAdd() {
                 this.skipDetail();
             },
-            onEdit(row) {
-                this.skipDetail(row.orderNo);
+            onEdit(orderNo) {
+                this.skipDetail(orderNo);
             },
             skipDetail(orderNo) {
                 this.$router.push({path: '/product/purchase/order/edit', query: {orderNo: orderNo}});
@@ -148,6 +168,32 @@
                     return "";
                 }
             },
+            getValByType(i) {
+              switch (i.type) {
+                case "String":
+                  return i["_string"];
+                case "Boolean":
+                  return i["_bool"];
+                case "ArrayList":
+                  return i["_array"].join(",");
+                case "Double":
+                  return i["_double"];
+                case "Float":
+                  return i["_float"];
+                case "Integer":
+                  return i["_int"];
+                case "Byte":
+                  return i["_byte"];
+                case "Short":
+                  return i["_short"];
+                case "Long":
+                  return i["_long"];
+                case "Date":
+                  return this.$moment(i["_date"]).format(i.format || "YYYY-MM-DD");
+                default:
+                  return i["_string"];
+              }
+            }
         },
         components: {
             search
