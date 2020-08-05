06ce7d5e321814ff1edcf30a328c9152ad4fc523
pipilin <2865993293@qq.com>
2020-08-05 15:36:53 +0800

    .

--- src/views/customer/Config.vue
+++ src/views/customer/Config.vue
@@ -1,67 +1,49 @@
 <template>
   <div class="page">
-    <div class="page-back">
-      <el-button-group>
-        <el-button icon="el-icon-back" type="warning" @click="goBack"
-          >返回</el-button
-        >
-        <el-button icon="el-icon-plus" type="primary" @click="handleSave"
-          >保存</el-button
-        >
-      </el-button-group>
-    </div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-row class="">
-        <el-col :span="6">
-          <el-form-item label="客户名称:">
-            <el-input v-model="form.name" style="width: 100%;"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="6">
-          <el-form-item label="平台名称:">
-            <el-input v-model="form.name" style="width: 100%"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-    </el-form>
-
-    <div class="content">
-      <el-row class="el-row-left">
-          <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAll">全选</el-checkbox>
-          <el-checkbox-group v-model="tabsCheckeds" @change="handleChange">
-            <el-checkbox v-for="tabsOption in tabsOptions" :label="tabsOption" :key="tabsOption" class="el-row-child">{{tabsOption}}</el-checkbox>
-          </el-checkbox-group>
-      </el-row>
-      <el-row class="el-row-right">
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-row class="el-row-item">
-            <el-col :span="24">
-              <el-form-item style="text-align:center;">
-                  <el-tag v-model="selTab">{{selTab}}</el-tag>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item>
-                <el-input placeholder="请输入导航名称" />
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item>
-                <el-input placeholder="请输入导航地址" />
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </el-form>
-      </el-row>
+    <div >
+      <el-tabs :tab-position="tabPosition" style="min-height: 200px;">
+        <el-tab-pane label="订单通知管理">
+          <div class="page-back">
+            <el-button-group>
+              <el-button icon="el-icon-back" type="warning" @click="goBack">返回</el-button>
+              <el-button icon="el-icon-plus" type="primary" @click="handleSave">保存</el-button>
+            </el-button-group>
+          </div>
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-row class="el-row-item">
+              <el-col :span="12">
+                <el-form-item label="域名:">
+                  <el-input placeholder="请输入导航名称" />
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="安全码：">
+                  <el-input placeholder="请输入导航地址" />
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="消息通知地址：">
+                  <el-input type="textarea" placeholder="请输入导航地址" />
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="订单接口管理">配置管理</el-tab-pane>
+        <el-tab-pane label="订单更新管理">角色管理</el-tab-pane>
+        <el-tab-pane label="政策配置管理">政策配置管理</el-tab-pane>
+        <el-tab-pane label="政策导入管理">政策导入管理</el-tab-pane>
+      </el-tabs>
+      <el-row class="el-row-right"> </el-row>
     </div>
   </div>
 </template>
 <script>
 export default {
-  methods:{
-    onClick(index){ 
-      this.current = index
-      console.log(index)
+  methods: {
+    onClick(index) {
+      this.current = index;
+      console.log(index);
     },
     goBack() {
       if (this.$router.history.length <= 1) {
@@ -71,15 +53,13 @@ export default {
         this.$router.go(-1);
       }
     },
-    handleSave(){
-
-    },
+    handleSave() {},
     handleCheckAll(val) {
       this.tabsCheckeds = val ? this.tabsOptions : [];
       this.isIndeterminate = false;
     },
-    handleChange(val) { 
-      if(val && val.length > 0){
+    handleChange(val) {
+      if (val && val.length > 0) {
         let num = val.length - 1;
         let selObj = val[num];
         this.current = this.getIndexByArray(this.tabsOptions, selObj);
@@ -87,27 +67,28 @@ export default {
       }
       let checkedCount = val.length;
       this.checkAll = checkedCount === this.tabsOptions.length;
-      this.isIndeterminate = checkedCount > 0 && checkedCount < this.tabsOptions.length;
+      this.isIndeterminate =
+        checkedCount > 0 && checkedCount < this.tabsOptions.length;
     },
-    getIndexByArray(array, selObj){
-       if(array && array.length > 0 && selObj){
-         let num = 0;
-         let index = -1;
-         array.forEach(obj => {
-           if(obj === selObj){
-             index = num;
-           }
-           num++;
-         });
-         return index;
-       } 
+    getIndexByArray(array, selObj) {
+      if (array && array.length > 0 && selObj) {
+        let num = 0;
+        let index = -1;
+        array.forEach(obj => {
+          if (obj === selObj) {
+            index = num;
+          }
+          num++;
+        });
+        return index;
+      }
     }
   },
   data() {
     return {
       tabPosition: "left",
-      current:0,
-      checked:false,
+      current: 0,
+      checked: false,
       form: {
         name: "",
         region: "",
@@ -116,18 +97,15 @@ export default {
         delivery: false,
         type: [],
         resource: "",
-        desc: "",
-      },
-      tabsOptions:["订单通知管理","订单接口管理","订单更新管理","政策配置管理","政策参数管理"],
-      tabsCheckeds:["订单通知管理"],
-      checkAll: false,
-      isIndeterminate: true,
-      selTab: "订单通知管理"
+        desc: ""
+      }
     };
-  },
+  }
 };
 </script>
 <style scoped>
+.page{
+}
 .el-row-item {
   border: 1px solid #ddd;
   border-radius: 6px;
@@ -138,28 +116,5 @@ export default {
 }
 .page-back {
   margin-bottom: 20px;
-}
-.content{
-  height: 500px;
-  /* display: flex;
-  justify-content: flex-start; */
-}
-.el-row-left {
-  width: auto;
-  line-height: 40px;
-  float: left;
-  border-right: 1px solid #ddd;
-  padding-right: 20px;
-}
-.el-row-left .el-row-child{
-  width: auto;
-  display: block;
-}
-.el-row-right {
-  height: auto;
-  float: left;
-}
-.active{
-  color: #409EFF;
 }
 </style>
