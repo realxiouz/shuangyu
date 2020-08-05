7b4bed8a13505d1c04daebc4aecff7b98c81fbb1
pipilin <2865993293@qq.com>
2020-08-05 17:22:34 +0800

    .

--- src/views/customer/Config.vue
+++ src/views/customer/Config.vue
@@ -1,95 +1,11 @@
 <template>
   <div class="page">
       <el-tabs :tab-position="tabPosition" style="min-height: 200px;">
-        <el-tab-pane label="订单通知管理">
-          <div class="page-back">
-            <el-button-group>
-              <el-button icon="el-icon-back" type="warning" @click="goBack">返回</el-button>
-              <el-button icon="el-icon-plus" type="primary" @click="handleSave">保存</el-button>
-            </el-button-group>
-          </div>
-          <el-form ref="form" :model="form" label-width="80px">
-            <el-row class="el-row-item">
-              <el-col :span="12">
-                <el-form-item label="域名:">
-                  <el-input placeholder="请输入域名" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="安全码：">
-                  <el-input placeholder="请输入安全码" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="消息通知地址：">
-                  <el-input type="textarea" placeholder="请输入消息通知地址" />
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </el-form>
-        </el-tab-pane>
-        <el-tab-pane label="订单接口管理">
-          <div class="page-back">
-            <el-button-group>
-              <el-button icon="el-icon-back" type="warning" @click="goBack">返回</el-button>
-              <el-button icon="el-icon-plus" type="primary" @click="handleSave">保存</el-button>
-            </el-button-group>
-          </div>
-          <el-form ref="form" :model="form" label-width="80px">
-            <el-row class="el-row-item">
-              <el-col :span="12">
-                <el-form-item label="域名:">
-                  <el-input placeholder="请输入域名" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="用户名:">
-                  <el-input placeholder="请输入用户名" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="密码:">
-                  <el-input placeholder="请输入密码" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="IP:">
-                  <el-input type="textarea" placeholder="请输入IP" />
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </el-form>
-        </el-tab-pane>
+        <el-tab-pane label="订单通知管理"></el-tab-pane>
+        <el-tab-pane label="订单接口管理"></el-tab-pane>
         <el-tab-pane label="订单更新管理">订单更新管理</el-tab-pane>
         <el-tab-pane label="政策配置管理">政策配置管理</el-tab-pane>
-        <el-tab-pane label="政策导入管理">
-          <div class="page-back">
-            <el-button-group>
-              <el-button icon="el-icon-back" type="warning" @click="goBack">返回</el-button>
-              <el-button icon="el-icon-plus" type="primary" @click="handleSave">保存</el-button>
-            </el-button-group>
-          </div>
-          <el-form ref="form" :model="form" label-width="80px">
-            <el-row class="el-row-item">
-              <el-col :span="12">
-                <el-form-item label="用户名称:">
-                  <el-input placeholder="请输入用户名称" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="ip地址:">
-                  <el-input placeholder="请输入ip地址" />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="回调地址:">
-                  <el-input placeholder="请输入回调地址" />
-                </el-form-item>
-              </el-col>
-              
-            </el-row>
-          </el-form>
-        </el-tab-pane>
+        <el-tab-pane label="政策导入管理"></el-tab-pane>
       </el-tabs>
     </div>
 </template>
